4d089b2347c1e6d9afe3b45156aa876d24e56c71
imdh <eastlake520@gmail.com>
2023-07-29 12:17:56 +0900

    setting: 프로젝트 초기 세팅

---
+++ src/main/java/softeer/mycarmaster/MyCarMasterApplication.java
@@ -0,0 +1,13 @@
+package softeer.mycarmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCarMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCarMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/softeer/mycarmaster/MyCarMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package softeer.mycarmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCarMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
