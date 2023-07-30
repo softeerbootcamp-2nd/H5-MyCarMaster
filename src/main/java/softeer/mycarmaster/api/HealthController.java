17786afabba89881955034790771c87cd552ddeb
imdh <eastlake520@gmail.com>
2023-07-30 09:51:58 +0900

    deploy: 로드밸런싱 정상 동작 확인

--- src/main/java/softeer/mycarmaster/api/HealthController.java
+++ src/main/java/softeer/mycarmaster/api/HealthController.java
@@ -3,11 +3,20 @@ package softeer.mycarmaster.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "ok";
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            String hostname = ip.getHostName();
+            return "Your current IP address : " + ip + ", Hostname : " + hostname;
+        } catch (UnknownHostException e) {
+            return "Error retrieving IP and Hostname";
+        }
     }
 }
