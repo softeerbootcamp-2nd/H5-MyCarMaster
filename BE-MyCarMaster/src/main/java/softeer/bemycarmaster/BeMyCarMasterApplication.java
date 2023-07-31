95c158c2920c9ab4572c2dc6c83e588eef388d86
imdh <eastlake520@gmail.com>
2023-07-31 23:29:00 +0900

    setting: 프로젝트 생성

---
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/BeMyCarMasterApplication.java
@@ -0,0 +1,13 @@
+package softeer.bemycarmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMyCarMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeMyCarMasterApplication.class, args);
+    }
+
+}
---
+++ BE-MyCarMaster/src/test/java/softeer/bemycarmaster/BeMyCarMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package softeer.bemycarmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMyCarMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
