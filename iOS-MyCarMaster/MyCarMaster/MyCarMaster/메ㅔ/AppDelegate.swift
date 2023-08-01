ec5ba446e482618b807f09fea77304a5678e3711
miniOS <seungmin.oh.dev@gmail.com>
2023-08-01 18:11:42 +0900

    setting: 스토리보드 제거

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/메ㅔ/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/01.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
