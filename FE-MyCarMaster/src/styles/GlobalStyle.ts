943c27e177327d70b33a99024357f01e384d9a9b
wonho1401 <wonho1401@naver.com>
2023-08-01 22:47:25 +0900

    design: GlobalStyle css 변경 body bg-color 변경

--- FE-MyCarMaster/src/styles/GlobalStyle.ts
+++ FE-MyCarMaster/src/styles/GlobalStyle.ts
@@ -18,8 +18,7 @@ const GlobalStyle = createGlobalStyle`
     body{
         line-height: 1;
         font-family: 'Noto Sans KR', sans-serif;
-        background-color: #F6F9F0;
-        margin-bottom: 100px;
+        background-color: #FFFFFF;
     }
     ol, ul{
         list-style: none;
