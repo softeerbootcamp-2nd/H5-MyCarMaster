ff3df895c5cee38c6c6f2cfe6c63b0ef74932039
ParkByunggwon <startartart@naver.com>
2023-08-03 02:54:57 +0900

    design: trim view 구성

--- FE-MyCarMaster/src/components/Main/view/TrimContent.tsx
+++ FE-MyCarMaster/src/components/Main/view/TrimContent.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import styled from "styled-components";
+
+// dummy data
+import exclusive from "../../../assets/images/exclusive.png";
+import leblanc from "../../../assets/images/leblanc.png";
 
 function TrimContent() {
-  return <div>TrimContent</div>;
+  return (
+    <>
+      <TrimImage src={exclusive} />
+    </>
+  );
 }
 
+const TrimImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  object-position: center;
+`;
 export default TrimContent;
