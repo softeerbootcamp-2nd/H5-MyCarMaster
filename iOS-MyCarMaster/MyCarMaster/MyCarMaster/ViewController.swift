ed9993c39677303921a61840f870998c381c11ce
miniOS <seungmin.oh.dev@gmail.com>
2023-08-02 10:18:47 +0900

    [Setting] 프로젝트 세팅 (#54)
    * chore: gitignore 추가
    
    * setting: 프로젝트 생성
    
    * setting: 프로젝트 배포 타겟, 설정 변경
    
    * 배포타겟 iOS 14.0으로 설정
    * iPhone만 대응
    * portrait만 대응
    
    * setting: 스토리보드 제거
    
    * chore: AppDelegate, SceneDelegate 불필요한 코드 삭제
    
    * setting: 프로젝트 구조 정리
    
    * setting: SwiftLint 추가
    
    * 로컬 swiftlint를 사용한다.
    
    * setting: SwiftLint 규칙 수정
    
    * setting: SwiftLint Auto 규칙 추가
    
    * chore: 기존 코드에 새로 적용된 SwiftLint 적용

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray
+    }
+}
