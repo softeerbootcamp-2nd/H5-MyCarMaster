5b188b756c05303f0538a85ccfba36be4872fc4c
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2023-08-02 15:57:50 +0900

    [Setting] code lint 설정 (#61)
    * setting: checkstyle 및 .editorconfig 설정
    
    * setting: workflow를 통해 pr 생성시 checkstyle 검증 설정
    
    * setting: job 나누지 않게 수정
    
    * setting: checkstyle 동작 테스트

--- BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/HealthController.java
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/HealthController.java
@@ -1,22 +1,22 @@
 package softeer.bemycarmaster.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        try {
-            InetAddress ip = InetAddress.getLocalHost();
-            String hostname = ip.getHostName();
-            return "Your current IP address : " + ip + ", Hostname : " + hostname;
-        } catch (UnknownHostException e) {
-            return "Error retrieving IP and Hostname";
-        }
-    }
+	@GetMapping("/health")
+	public String healthCheck() {
+		try {
+			InetAddress ip = InetAddress.getLocalHost();
+			String hostname = ip.getHostName();
+			return "Your current IP address : " + ip + ", Hostname : " + hostname;
+		} catch (UnknownHostException e) {
+			return "Error retrieving IP and Hostname";
+		}
+	}
 }
--- BE-MyCarMaster/src/test/java/softeer/bemycarmaster/BeMyCarMasterApplicationTests.java
+++ BE-MyCarMaster/src/test/java/softeer/bemycarmaster/BeMyCarMasterApplicationTests.java
@@ -1,7 +1,7 @@
 package softeer.bemycarmaster;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BeMyCarMasterApplicationTests {}
+class BeMyCarMasterApplicationTests {
+}
