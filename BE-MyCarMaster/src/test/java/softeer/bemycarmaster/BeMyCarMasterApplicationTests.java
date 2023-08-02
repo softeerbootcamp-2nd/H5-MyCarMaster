29fa717fa3a07358759a9de17d9325f9462a3fc9
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2023-08-02 14:00:11 +0900

    [Setting] 개발환경 세팅 (#52)
    * setting: 파일명 및 branch명 수정
    
    * setting: gradlew path 수정
    
    * setting: build with gradlew path 수정
    
    * setting: build시 cd로 디렉토리에 들어가서 실행
    
    * setting: env file 분리 및 submodule 설정
    
    * setting: docker 관련 세팅
    
    * setting: env local, prod 환경으로 관리
    
    ---------
    
    Co-authored-by: jangdongho <ehdgh25@naver.com>

--- BE-MyCarMaster/src/test/java/softeer/bemycarmaster/BeMyCarMasterApplicationTests.java
+++ BE-MyCarMaster/src/test/java/softeer/bemycarmaster/BeMyCarMasterApplicationTests.java
@@ -4,10 +4,4 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BeMyCarMasterApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+class BeMyCarMasterApplicationTests {}
