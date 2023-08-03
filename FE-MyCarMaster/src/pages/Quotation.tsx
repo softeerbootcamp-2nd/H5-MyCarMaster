aa528df1a330c64cb78aff04a151e8ad3258482a
ParkByunggwon <startartart@naver.com>
2023-08-03 15:13:13 +0900

    design: 견적서 완성 페이지 구성

---
+++ FE-MyCarMaster/src/pages/Quotation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function Quotation() {
+  return <Container>Quotation</Container>;
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80rem;
+  height: 45rem;
+  margin: 0 8rem;
+`;
+
+export default Quotation;
