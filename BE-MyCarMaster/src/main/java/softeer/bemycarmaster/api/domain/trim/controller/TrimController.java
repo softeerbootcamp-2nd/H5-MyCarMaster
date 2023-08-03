9df75935245f5d1b0cb96a066d7d1fcfb8220d1a
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-03 17:45:01 +0900

    feat: TrimContoller 추가 및 getTrims 구현

---
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/trim/controller/TrimController.java
@@ -0,0 +1,32 @@
+package softeer.bemycarmaster.api.domain.trim.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import softeer.bemycarmaster.api.domain.trim.dto.request.GetTrimsRequest;
+import softeer.bemycarmaster.api.domain.trim.dto.response.GetTrimsResponse;
+import softeer.bemycarmaster.api.domain.trim.usecase.GetTrimsUseCase;
+import softeer.bemycarmaster.api.global.response.Response;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/trims")
+@Tag(name = "Trim", description = "Trim API Document")
+public class TrimController {
+
+	private final GetTrimsUseCase getTrimsUseCase;
+
+	@GetMapping
+	@Operation(summary = "모델에 따른 트림 목록을 반환합니다")
+	public Response<GetTrimsResponse> getTrims(@RequestBody GetTrimsRequest getTrimsRequest) {
+
+		Integer modelId = getTrimsRequest.getModelId();
+		GetTrimsResponse getTrimsResponse = getTrimsUseCase.execute(modelId);
+		return new Response<>(getTrimsResponse);
+	}
+}
