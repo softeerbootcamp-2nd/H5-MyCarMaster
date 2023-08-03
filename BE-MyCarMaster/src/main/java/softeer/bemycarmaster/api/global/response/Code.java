5e2a98eedc8f436582f2fa61ae7dd237ca8b4a0e
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-03 13:34:53 +0900

    feat: 요청 처리 상태 코드 및 메시지 관리를 위한 enum Code 구현

---
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/global/response/Code.java
@@ -0,0 +1,17 @@
+package softeer.bemycarmaster.api.global.response;
+
+import lombok.Getter;
+
+@Getter
+public enum Code {
+
+	SUCCESS(2000, "요청에 성공하셨습니다.");
+
+	private int code;
+	private String message;
+
+	Code(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+}
