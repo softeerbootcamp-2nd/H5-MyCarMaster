4cbc959885f4d465d85421be7a1ecdd38cb935a5
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-03 17:43:10 +0900

    feat: Trim dto 구현

---
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/trim/dto/response/Trim.java
@@ -0,0 +1,28 @@
+package softeer.bemycarmaster.api.domain.trim.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Trim {
+
+	@Schema(description = "트림 식별자", example = "1")
+	private Integer id;
+
+	@Schema(description = "트림명", example = "Le Blanc")
+	private String name;
+
+	@Schema(description = "트림 설명", example = "Le Blanc Trim Description")
+	private String description;
+
+	@Schema(description = "트림 기본 가격", example = "47720000")
+	private Integer price;
+
+	@Schema(description = "판매율", example = "22")
+	private Integer ratio;
+
+	@Schema(description = "트림 이미지", example = "s3 url")
+	private String imgUrl;
+}
