86fe394e6e31d1bba76a31071474ba6fd724fdaa
miniOS <seungmin.oh.dev@gmail.com>
2023-08-03 15:44:11 +0900

    feat: ColorAsset 추가 및 프로젝트에 필요한 Color 선언

--- iOS-MyCarMaster/MCMResource/MCMResource/Resource.swift
+++ iOS-MyCarMaster/MCMResource/MCMResource/Resource.swift
@@ -17,4 +17,21 @@ public enum Resource {
     public enum Font {
         
     }
+
+    public enum Color {
+        public static let black = RawColor.black.color as UIColor
+        public static let white = RawColor.white.color as UIColor
+        public static let coolGrey1 = RawColor.coolGrey1.color as UIColor
+        public static let coolGrey2 = RawColor.coolGrey2.color as UIColor
+        public static let grey1 = RawColor.grey1.color as UIColor
+        public static let grey2 = RawColor.grey2.color as UIColor
+        public static let grey3 = RawColor.grey3.color as UIColor
+        public static let navyBlue1 = RawColor.navyBlue1.color as UIColor
+        public static let navyBlue2 = RawColor.navyBlue2.color as UIColor
+        public static let navyBlue3 = RawColor.navyBlue3.color as UIColor
+        public static let navyBlue4 = RawColor.navyBlue4.color as UIColor
+        public static let navyBlue5 = RawColor.navyBlue5.color as UIColor
+        public static let gold1 = RawColor.gold1.color as UIColor
+        public static let gold2 = RawColor.gold2.color as UIColor
+    }
 }
