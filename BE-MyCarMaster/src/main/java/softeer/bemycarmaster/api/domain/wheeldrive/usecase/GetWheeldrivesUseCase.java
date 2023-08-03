73dc3ebf855265ac651fc208a2ceeaece2284bdf
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-03 22:11:48 +0900

    feat: GetWheeldrivesUseCase 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/wheeldrive/usecase/GetWheeldrivesUseCase.java
@@ -0,0 +1,13 @@
+package softeer.bemycarmaster.api.domain.wheeldrive.usecase;
+
+import softeer.bemycarmaster.api.domain.wheeldrive.dto.response.GetWheeldrivesResponse;
+import softeer.bemycarmaster.api.global.annotation.UseCase;
+
+@UseCase
+public class GetWheeldrivesUseCase {
+
+	public GetWheeldrivesResponse execute(Integer modelId, Integer trimId) {
+
+		return null;
+	}
+}
