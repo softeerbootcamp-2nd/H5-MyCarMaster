d11ebf0b339120e099a25c6a5847e1eeac8a3cb9
miniOS <seungmin.oh.dev@gmail.com>
2023-08-04 10:33:12 +0900

    [Feature] Onboarding에서 현대차 영상을 보고, Home으로 넘어갈 수 있다. (#100)
    * setting: lottie-ios Dependency 추가
    
    * Onboarding에서 Lottie 영상을 보여주기 위해서 라이브러리 추가
    
    * feat: Onboarding 구현
    
    * Onboarding UI 구현
    * ViewController와 기능 연결

--- iOS-MyCarMaster/MyCarMaster/MyCarMaster/App/SceneDelegate.swift
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/App/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = OnboardingViewController()
         window?.makeKeyAndVisible()
     }
 }
---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  OnboardingViewController.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/04.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+    private var onboardingView: OnboardingView {
+        return view as? OnboardingView ?? OnboardingView()
+    }
+
+    override func loadView() {
+        super.loadView()
+        view = OnboardingView(frame: .zero)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        onboardingView.addTarget(keyPath: \.startButton, action: #selector(startButtonDidTap(_:)), for: .touchUpInside)
+        onboardingView.playLottie()
+    }
+
+    @objc
+    func startButtonDidTap(_ sender: UIButton) {
+        // TODO: Routing 레이어에서 RootViewController 교체
+        let viewController = ViewController()
+        viewController.modalPresentationStyle = .fullScreen
+        present(viewController, animated: false)
+    }
+}
