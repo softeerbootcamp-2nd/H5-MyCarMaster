39d89f57d501632f1e749453b3aca76f6e0f6602
imdh <eastlake520@gmail.com>
2023-08-04 22:45:03 +0900

    [Feature] 추가 옵션 목록 조회 swagger 문서 작성 (#97)
    * feat: OptionController 구현
    
    * feat: Option 추가
    
    * feat: GetOptionsResponse 추가
    
    key 값을 String으로 할지, OptionEnum으로 할지 논의 필요
    
    * feat: PackageOptionDetail 추가
    
    * feat: GetOptionsRequest 구현
    
    * feat: GetOptionsUseCase 추가
    
    * chore: Option의 price 설명을 가격 -> 비용으로 변경
    
    * chore: PackageOptionDetail -> SubOption 으로 네이밍 변경
    
    * feat: 요구사항 구체화에 따른 response 추가
    
    프론트사이드 필터링으로 변경함에 따라 Option에 category, tag 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/option/dto/request/GetOptionsRequest.java
@@ -0,0 +1,19 @@
+package softeer.bemycarmaster.api.domain.option.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetOptionsRequest {
+
+	@Schema(description = "모델 식별자", example = "1")
+	private Integer modelId;
+
+	@Schema(description = "트림 식별자", example = "1")
+	private Integer trimId;
+
+	@Schema(description = "엔진 식별자", example = "1")
+	private Integer engineId;
+}
