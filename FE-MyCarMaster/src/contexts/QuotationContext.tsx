30f348a9b6d96c5b0f4406925bef0f94b611f344
ParkByunggwon <startartart@naver.com>
2023-08-05 18:34:46 +0900

    feat: QuotationContext 구현 ContextAPI, useReducer를 사용해 state, dispatch, provider 생성

---
+++ FE-MyCarMaster/src/contexts/QuotationContext.tsx
@@ -0,0 +1,120 @@
+import { useReducer, createContext, useContext } from "react";
+
+type quotation = {
+  name: string;
+  price: number;
+  imgUrl?: string;
+  category?: string;
+  description?: string;
+};
+
+type QuotationState = {
+  navigationId: number;
+  isFirst: boolean[];
+  trimQuotation: quotation[];
+  engineQuotation: quotation[];
+  wheelDriveQuotation: quotation[];
+  bodyTypeQuotation: quotation[];
+  exteriorColorQuotation: quotation[];
+  interiorColorQuotation: quotation[];
+  selectedQuotation: quotation[];
+  consideredQuotation: quotation[];
+};
+
+type QuotationAction = {
+  type: "NAVIGATE" | "SET_QUOTATION";
+  payload: {
+    navigationId: number;
+    isFirst: boolean[];
+    trimQuotation: quotation[];
+    engineQuotation: quotation[];
+    wheelDriveQuotation: quotation[];
+    bodyTypeQuotation: quotation[];
+    exteriorColorQuotation: quotation[];
+    interiorColorQuotation: quotation[];
+    selectedQuotation: quotation[];
+    consideredQuotation: quotation[];
+  };
+};
+
+const initialQuotationState: QuotationState = {
+  navigationId: 0,
+  isFirst: [true, true, true, true, true, true, true],
+  trimQuotation: [],
+  engineQuotation: [],
+  wheelDriveQuotation: [],
+  bodyTypeQuotation: [],
+  exteriorColorQuotation: [],
+  interiorColorQuotation: [],
+  selectedQuotation: [],
+  consideredQuotation: [],
+};
+
+type QuotationDispatch = (action: QuotationAction) => void;
+
+const quotationReducer = (
+  state: QuotationState,
+  action: QuotationAction
+): QuotationState => {
+  switch (action.type) {
+    case "NAVIGATE":
+      return {
+        ...state,
+        navigationId: action.payload.navigationId,
+        isFirst: action.payload.isFirst,
+      };
+    case "SET_QUOTATION":
+      return {
+        ...state,
+        trimQuotation: action.payload.trimQuotation,
+        engineQuotation: action.payload.engineQuotation,
+        wheelDriveQuotation: action.payload.wheelDriveQuotation,
+        bodyTypeQuotation: action.payload.bodyTypeQuotation,
+        exteriorColorQuotation: action.payload.exteriorColorQuotation,
+        interiorColorQuotation: action.payload.interiorColorQuotation,
+        selectedQuotation: action.payload.selectedQuotation,
+        consideredQuotation: action.payload.consideredQuotation,
+      };
+    default:
+      return state;
+  }
+};
+
+const QuotationStateContext = createContext<QuotationState | undefined>(
+  undefined
+);
+const QuotationDispatchContext = createContext<QuotationDispatch | undefined>(
+  undefined
+);
+
+export const QuotationProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [state, dispatch] = useReducer(quotationReducer, initialQuotationState);
+  return (
+    <QuotationStateContext.Provider value={state}>
+      <QuotationDispatchContext.Provider value={dispatch}>
+        {children}
+      </QuotationDispatchContext.Provider>
+    </QuotationStateContext.Provider>
+  );
+};
+
+export const useQuotationState = () => {
+  const state = useContext(QuotationStateContext);
+  if (!state) throw new Error("Cannot find QuotationProvider");
+  return state;
+};
+
+export const useQuotationDispatch = () => {
+  const dispatch = useContext(QuotationDispatchContext);
+  if (!dispatch) throw new Error("Cannot find QuotationProvider");
+  return dispatch;
+};
+
+export const useQuotation = () =>
+  [useQuotationState(), useQuotationDispatch()] as const;
+
+export default QuotationProvider;
