e4f4ae6bf6f27d85eb8ed606129f8173a1f3a988
ParkByunggwon <startartart@naver.com>
2023-08-05 19:21:58 +0900

    refactor: navgationId 적용 Props로 전달받는 navIndex를 전역 id로 변경

--- FE-MyCarMaster/src/components/Main/MainWrapper.tsx
+++ FE-MyCarMaster/src/components/Main/MainWrapper.tsx
@@ -2,16 +2,14 @@ import TrimContent from "./view/TrimContent";
 import DetailModelContent from "./view/DetailModelContent";
 import ColorContent from "./view/ColorContent";
 import OptionContent from "./view/OptionContent";
+import { useQuotationState } from "../../contexts/QuotationContext";
 
-interface MainWrapperProps {
-  navIndex: number;
-}
-
-function MainWrapper({ navIndex }: MainWrapperProps) {
-  if (navIndex === 0) return <TrimContent />;
-  if (navIndex === 1) return <DetailModelContent />;
-  if (navIndex === 2) return <ColorContent />;
-  if (navIndex === 3) return <OptionContent />;
+function MainWrapper() {
+  const { navigationId } = useQuotationState();
+  if (navigationId === 0) return <TrimContent />;
+  if (navigationId === 1) return <DetailModelContent />;
+  if (navigationId === 2) return <ColorContent />;
+  if (navigationId === 3) return <OptionContent />;
   return <></>;
 }
 
