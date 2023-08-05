6a2eb1fce7cc268b5803893cf311b59fadc78b67
imdh <eastlake520@gmail.com>
2023-08-05 22:48:56 +0900

    feat: ResponseStatus에 상태값 추가
    클라이언트 예외와 서버 에러 응답을 위한 Status 추가

--- BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/global/response/ResponseStatus.java
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/global/response/ResponseStatus.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ResponseStatus {
 
-	SUCCESS(2000, "요청에 성공하셨습니다.");
+	SUCCESS(2000, "요청에 성공하셨습니다."),
+
+	BAD_REQUEST(4000, "잘못된 요청입니다"),
+
+	INTERNAL_SERVER_ERROR(5000, "서버 내부에서 문제가 발생했습니다.");
 
 	private final int code;
 
