389656776a0d61d8d0f4e269e211f965e0050c1b
imdh <eastlake520@gmail.com>
2023-08-06 21:18:58 +0900

    feat: GetModelsUseCase 구현

--- BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/model/usecase/GetModelsUseCase.java
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/model/usecase/GetModelsUseCase.java
@@ -1,13 +1,21 @@
 package softeer.bemycarmaster.api.domain.model.usecase;
 
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import softeer.bemycarmaster.api.domain.model.domain.Model;
 import softeer.bemycarmaster.api.domain.model.dto.response.GetModelsResponse;
+import softeer.bemycarmaster.api.domain.model.usecase.port.ModelPort;
 import softeer.bemycarmaster.api.global.annotation.UseCase;
 
 @UseCase
+@RequiredArgsConstructor
 public class GetModelsUseCase {
 
+	private final ModelPort modelPort;
+
 	public GetModelsResponse execute() {
-		// Mapper 고려
-		return null;
+		List<Model> models = modelPort.findModels();
+		return GetModelsResponse.from(models);
 	}
 }
