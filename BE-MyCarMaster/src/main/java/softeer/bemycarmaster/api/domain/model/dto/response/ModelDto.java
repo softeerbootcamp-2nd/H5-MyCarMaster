d521ad7d56655733c629c3b224f5c4f0103e190d
imdh <eastlake520@gmail.com>
2023-08-06 20:22:23 +0900

    chore: AllArgsConstructor 접근지정자 설정
    private 설정

--- BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/model/dto/response/ModelDto.java
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/model/dto/response/ModelDto.java
@@ -1,12 +1,17 @@
 package softeer.bemycarmaster.api.domain.model.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Model {
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ModelDto {
 
 	@Schema(description = "모델 식별자", example = "1")
 	private Integer id;
