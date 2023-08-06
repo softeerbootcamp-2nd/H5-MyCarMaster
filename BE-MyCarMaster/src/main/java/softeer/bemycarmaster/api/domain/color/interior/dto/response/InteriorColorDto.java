8f1e2fd9b4ed89244e4c072ee19683159ce6d980
imdh <eastlake520@gmail.com>
2023-08-06 19:39:03 +0900

    chore: InteriorColor -> InteriorColorDto로 네이밍 변경

--- BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/color/interior/dto/response/InteriorColorDto.java
+++ BE-MyCarMaster/src/main/java/softeer/bemycarmaster/api/domain/color/interior/dto/response/InteriorColorDto.java
@@ -1,12 +1,16 @@
 package softeer.bemycarmaster.api.domain.color.interior.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class InteriorColor {
+@Builder
+@AllArgsConstructor
+public class InteriorColorDto {
 
 	@Schema(description = "내장 색상 식별자", example = "1")
 	private Integer id;
