3c725f9c8719259020b092d173da6d011a527be5
wonho <59119468+wonho1401@users.noreply.github.com>
2023-08-07 21:52:10 +0900

    [Feature] 외장 색상 뷰, 내장 색상 뷰 구성 (#126)
    * feat: 외부, 내부 색상 뷰 구성
    
    * chore: 외장, 내장 색상 예시 이미지 추가
    
    * chore: navigationId 추가하여 렌더링 추가
    navIndex -> navigationId by QuotationState
    
    * chore: 외장 색상 테스트 이미지 추가
    
    * chore: 이미지 경로 변경
    상대경로 -> 절대경로

---
+++ FE-MyCarMaster/src/components/Main/view/ColorView/ExteriorColorView.tsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+function ExteriorColorView() {
+  return <ExteriorColorImg src="/images/exterior/black/image_1.png" />;
+}
+
+const ExteriorColorImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  object-position: center;
+`;
+
+export default ExteriorColorView;
