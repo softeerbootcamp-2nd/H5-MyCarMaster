eda1aaa1bea552f90a26311bb6d6074e9bc1dad6
ParkByunggwon <startartart@naver.com>
2023-08-07 18:29:18 +0900

    chore: QuotationContext 더미 데이터 수정

--- FE-MyCarMaster/src/contexts/QuotationContext.tsx
+++ FE-MyCarMaster/src/contexts/QuotationContext.tsx
@@ -24,22 +24,22 @@ const initialQuotationState: QuotationState = {
   navigationId: 0,
   isFirst: [false, true, true, true, true, true, true],
   trimQuotation: {
-    trimQuotation: { name: "Le Blanc (르블랑)", price: 1000 },
+    trimQuotation: { name: "Le Blanc (르블랑)", price: 10000000 },
   },
   detailQuotation: {
-    engineQuotation: { name: "가솔린3.8", price: 20 },
-    wheelDriveQuotation: { name: "4WD", price: 30 },
-    bodyTypeQuotation: { name: "7인승", price: 40 },
+    engineQuotation: { name: "가솔린3.8", price: 20000 },
+    wheelDriveQuotation: { name: "4WD", price: 30000 },
+    bodyTypeQuotation: { name: "7인승", price: 40500 },
   },
   carPaintQuotation: {
-    exteriorColorQuotation: { name: "그라파이드 그레이 메탈릭", price: 50 },
-    interiorColorQuotation: { name: "퀼팅천연(블랙)", price: 60 },
+    exteriorColorQuotation: { name: "그라파이드 그레이 메탈릭", price: 50700 },
+    interiorColorQuotation: { name: "퀼팅천연(블랙)", price: 60400 },
   },
   optionQuotation: {
     selectedQuotation: [
-      { name: "파노라마 선루프", price: 70 },
-      { name: "헤드업 디스플레이", price: 80 },
-      { name: "하이패스", price: 90 },
+      { name: "파노라마 선루프", price: 70200 },
+      { name: "헤드업 디스플레이", price: 84000 },
+      { name: "하이패스", price: 90110 },
     ],
     consideredQuotation: [
       { name: "파노라마 선루프", price: 70 },
@@ -62,8 +62,8 @@ const quotationReducer = (
         navigationId:
           action.payload.navigationId === -1
             ? 0
-            : action.payload.navigationId === 7
-            ? 6
+            : action.payload.navigationId === 8
+            ? 7
             : action.payload.navigationId,
         isFirst: action.payload.isFirst
           ? action.payload.isFirst
