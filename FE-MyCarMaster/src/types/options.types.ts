37d1dfb72fb1a213f2b6fe7481bad7c48c195437
wonho1401 <wonho1401@naver.com>
2023-08-08 19:04:47 +0900

    chore: option에 필요한 타입 파일 변경

--- FE-MyCarMaster/src/types/options.types.ts
+++ FE-MyCarMaster/src/types/options.types.ts
@@ -15,3 +15,24 @@ export type SuboptionsType = {
   description: string;
   imgUrl?: string;
 };
+
+export type OptionCategoryType = {
+  [key: string]: string;
+};
+
+export type OptionState = {
+  selectedOption: number[];
+  consideredOption: number[];
+  optionList: OptionType[];
+  optionCategoryId: number;
+};
+
+export type OptionAction = {
+  type: "SELECT_OPTION" | "SET_OPTION_LIST" | "SET_OPTION_CATEGORY_INDEX";
+  payload: {
+    selectedOption?: number[];
+    consideredOption?: number[];
+    optionList?: OptionType[];
+    optionCategoryId?: number;
+  };
+};
