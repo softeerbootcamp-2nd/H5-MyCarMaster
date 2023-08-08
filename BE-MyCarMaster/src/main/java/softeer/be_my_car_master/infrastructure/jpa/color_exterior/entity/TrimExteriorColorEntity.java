5e176c43acb98c59e49ddbc6f45d720df7993904
jangdongho <ehdgh25@naver.com>
2023-08-08 18:43:30 +0900

    feat: 외장 색상 관련 Entity 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/color_exterior/entity/TrimExteriorColorEntity.java
@@ -0,0 +1,49 @@
+package softeer.be_my_car_master.infrastructure.jpa.color_exterior.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import softeer.be_my_car_master.domain.color_exterior.ExteriorColor;
+import softeer.be_my_car_master.infrastructure.jpa.trim.entity.TrimEntity;
+
+@Entity
+public class TrimExteriorColorEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "ratio", nullable = false)
+	private Integer ratio;
+
+	@Column(name = "price", nullable = false)
+	private Integer price;
+
+	@Column(name = "colored_car_img_url", nullable = false)
+	private String coloredCarImgUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "trim_id")
+	private TrimEntity trim;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "exterior_color_id")
+	private ExteriorColorEntity exteriorColor;
+
+	public ExteriorColor toExteriorColor() {
+		return ExteriorColor.builder()
+			.id(exteriorColor.getId())
+			.name(exteriorColor.getName())
+			.ratio(ratio)
+			.price(price)
+			.colorImgUrl(exteriorColor.getColorImgUrl())
+			.coloredImgUrl(coloredCarImgUrl)
+			.build();
+	}
+}
