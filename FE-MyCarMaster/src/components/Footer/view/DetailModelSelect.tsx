ec2eecd45a75421d1fd9f870c84466f02ab9839d
ParkByunggwon <startartart@naver.com>
2023-08-09 00:27:52 +0900

    feat: DetailModelSelect 네비게이션 연동 및 제어

--- FE-MyCarMaster/src/components/Footer/view/DetailModelSelect.tsx
+++ FE-MyCarMaster/src/components/Footer/view/DetailModelSelect.tsx
@@ -1,11 +1,71 @@
 import styled from "styled-components";
-import { useDetailState } from "../../../contexts/DetailContext";
-import { useQuotationState } from "../../../contexts/QuotationContext";
+import {
+  useDetailState,
+  useDetailDispatch,
+} from "../../../contexts/DetailContext";
+import {
+  useQuotationState,
+  useQuotationDispatch,
+} from "../../../contexts/QuotationContext";
 import OptionBox from "../../../components/common/OptionBox/OptionBox";
 
 export default function DetailModelSelect() {
   const { engineList, wheelDriveList, bodyTypeList } = useDetailState();
   const { navigationId } = useQuotationState();
+  const detailDispatch = useDetailDispatch();
+  const quotationDispatch = useQuotationDispatch();
+
+  const selectEngine = (id: number) => {
+    quotationDispatch({
+      type: "SET_DETAIL_QUOTATION",
+      payload: {
+        type: "engineQuotation",
+        name: engineList[id].name,
+        price: engineList[id].price,
+      },
+    });
+    detailDispatch({
+      type: "SELECT_DETAIL",
+      payload: {
+        engineId: id,
+      },
+    });
+  };
+
+  const selectWheelDrive = (id: number) => {
+    quotationDispatch({
+      type: "SET_DETAIL_QUOTATION",
+      payload: {
+        type: "wheelDriveQuotation",
+        name: wheelDriveList[id].name,
+        price: wheelDriveList[id].price,
+      },
+    });
+    detailDispatch({
+      type: "SELECT_DETAIL",
+      payload: {
+        wheelDriveId: id,
+      },
+    });
+  };
+
+  const selectBodyType = (id: number) => {
+    quotationDispatch({
+      type: "SET_DETAIL_QUOTATION",
+      payload: {
+        type: "bodyTypeQuotation",
+        name: bodyTypeList[id].name,
+        price: bodyTypeList[id].price,
+      },
+    });
+    detailDispatch({
+      type: "SELECT_DETAIL",
+      payload: {
+        bodyTypeId: id,
+      },
+    });
+  };
+
   return (
     <Container>
       {navigationId === 1 && (
@@ -13,12 +73,14 @@ export default function DetailModelSelect() {
           {engineList.map((engine) => {
             return (
               <OptionBox
+                key={engine.id}
                 $id={engine.id}
                 $name={engine.name}
                 $description={engine.description}
                 $imgUrl={engine.imgUrl}
                 $ratio={engine.ratio}
                 $price={engine.price}
+                handleClick={() => selectEngine(engine.id)}
               />
             );
           })}
@@ -29,12 +91,14 @@ export default function DetailModelSelect() {
           {wheelDriveList.map((wheelDrive) => {
             return (
               <OptionBox
+                key={wheelDrive.id}
                 $id={wheelDrive.id}
                 $name={wheelDrive.name}
                 $description={wheelDrive.description}
                 $imgUrl={wheelDrive.imgUrl}
                 $ratio={wheelDrive.ratio}
                 $price={wheelDrive.price}
+                handleClick={() => selectWheelDrive(wheelDrive.id)}
               />
             );
           })}
@@ -45,12 +109,14 @@ export default function DetailModelSelect() {
           {bodyTypeList.map((bodyType) => {
             return (
               <OptionBox
+                key={bodyType.id}
                 $id={bodyType.id}
                 $name={bodyType.name}
                 $description={bodyType.description}
                 $imgUrl={bodyType.imgUrl}
                 $ratio={bodyType.ratio}
                 $price={bodyType.price}
+                handleClick={() => selectBodyType(bodyType.id)}
               />
             );
           })}
