ac5785b3ab5c6702cd05f96369a05e9ba12de0ba
imdh <eastlake520@gmail.com>
2023-08-09 00:24:24 +0900

    feat: UnselectableExteriorColorInteriorColorEntity 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/color_interior/entity/UnselectableExteriorColorInteriorColorEntity.java
@@ -0,0 +1,32 @@
+package softeer.be_my_car_master.infrastructure.jpa.color_interior.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import softeer.be_my_car_master.infrastructure.jpa.color_exterior.entity.ExteriorColorEntity;
+
+@Entity
+public class UnselectableExteriorColorInteriorColorEntity {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exterior_color_id")
+    private ExteriorColorEntity exteriorColor;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "interior_color_id")
+	private InteriorColorEntity interiorColor;
+
+	public Long getInteriorColorId() {
+		return interiorColor.getId();
+	}
+}
+
