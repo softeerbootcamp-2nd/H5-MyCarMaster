0bbfaae55118d7fec1c924bcde6c9c629281a7ab
imdh <eastlake520@gmail.com>
2023-08-09 00:03:40 +0900

    feat: InteriorColorPort 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/color_interior/usecase/port/InteriorColorPort.java
@@ -0,0 +1,12 @@
+package softeer.be_my_car_master.api.color_interior.usecase.port;
+
+import java.util.List;
+
+import softeer.be_my_car_master.domain.color_interior.InteriorColor;
+import softeer.be_my_car_master.global.annotation.Port;
+
+@Port
+public interface InteriorColorPort {
+
+	List<InteriorColor> findSelectableInteriorColorsByTrimId(Long trimId);
+}
