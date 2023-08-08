d53bc53981fef2ddcb8641bfdbc9ea4e08d983d4
wonho1401 <wonho1401@naver.com>
2023-08-08 19:04:32 +0900

    chore: 옵션에 필요한 constants 파일 생성

---
+++ FE-MyCarMaster/src/constants/Option.constants.ts
@@ -0,0 +1,16 @@
+import { OptionCategoryType } from "../types/options.types";
+
+export const categoryObject: OptionCategoryType = {
+  SAFE: "안전",
+  STYLE_PERFORMANCE: "스타일&퍼포먼스",
+  CAR_PROTECTION: "차량 보호",
+  CONVENIENCE: "편의",
+};
+
+export const categories = [
+  "전체",
+  "안전",
+  "스타일&퍼포먼스",
+  "차량 보호",
+  "편의",
+];
