7a9e963306549d8c48a6afc1c1092bad3f9fa467
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-08 18:32:33 +0900

    feat: BodyTypePort 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/body_type/usecase/port/BodyTypePort.java
@@ -0,0 +1,12 @@
+package softeer.be_my_car_master.api.body_type.usecase.port;
+
+import java.util.List;
+
+import softeer.be_my_car_master.domain.body_type.BodyType;
+import softeer.be_my_car_master.global.annotation.Port;
+
+@Port
+public interface BodyTypePort {
+
+	List<BodyType> findBodyTypes(Long modelId);
+}
