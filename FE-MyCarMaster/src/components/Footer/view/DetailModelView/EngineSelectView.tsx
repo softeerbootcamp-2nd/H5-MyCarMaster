91728973938da8376bbd4a7d8e58b696d8568ccd
wonho <59119468+wonho1401@users.noreply.github.com>
2023-08-09 17:13:15 +0900

    fix: 세부모델 payload 수정 (#146)

--- FE-MyCarMaster/src/components/Footer/view/DetailModelView/EngineSelectView.tsx
+++ FE-MyCarMaster/src/components/Footer/view/DetailModelView/EngineSelectView.tsx
@@ -1,49 +1,51 @@
 import {
-    useDetailState,
-    useDetailDispatch,
-  } from "../../../../contexts/DetailContext";
-  import { useQuotationDispatch } from "../../../../contexts/QuotationContext";
-  import OptionBox from "../../../common/OptionBox/OptionBox";
-  
-  export default function BodyTypeSelectView() {
-    const { engineList } = useDetailState();
-  
-    const detailDispatch = useDetailDispatch();
-    const quotationDispatch = useQuotationDispatch();
-  
-    const selectEngine = (id: number) => {
-      quotationDispatch({
-        type: "SET_DETAIL_QUOTATION",
-        payload: {
-          type: "engineQuotation",
-          name: engineList[id].name,
-          price: engineList[id].price,
-        },
-      });
-      detailDispatch({
-        type: "SELECT_DETAIL",
-        payload: {
-          engineId: id,
-        },
-      });
-    };
-  
-    return (
-      <>
-        {engineList.map((engine) => {
-          return (
-            <OptionBox
-              key={engine.id}
-              $id={engine.id}
-              $name={engine.name}
-              $description={engine.description}
-              $imgUrl={engine.imgUrl}
-              $ratio={engine.ratio}
-              $price={engine.price}
-              handleClick={() => selectEngine(engine.id)}
-            />
-          );
-        })}
-      </>
-    );
-  }
+  useDetailState,
+  useDetailDispatch,
+} from "../../../../contexts/DetailContext";
+import { useQuotationDispatch } from "../../../../contexts/QuotationContext";
+import OptionBox from "../../../common/OptionBox/OptionBox";
+
+export default function BodyTypeSelectView() {
+  const { engineList, wheelDriveId, bodyTypeId } = useDetailState();
+
+  const detailDispatch = useDetailDispatch();
+  const quotationDispatch = useQuotationDispatch();
+
+  const selectEngine = (id: number) => {
+    quotationDispatch({
+      type: "SET_DETAIL_QUOTATION",
+      payload: {
+        type: "engineQuotation",
+        name: engineList[id].name,
+        price: engineList[id].price,
+      },
+    });
+    detailDispatch({
+      type: "SELECT_DETAIL",
+      payload: {
+        engineId: id,
+        wheelDriveId,
+        bodyTypeId,
+      },
+    });
+  };
+
+  return (
+    <>
+      {engineList.map((engine) => {
+        return (
+          <OptionBox
+            key={engine.id}
+            $id={engine.id}
+            $name={engine.name}
+            $description={engine.description}
+            $imgUrl={engine.imgUrl}
+            $ratio={engine.ratio}
+            $price={engine.price}
+            handleClick={() => selectEngine(engine.id)}
+          />
+        );
+      })}
+    </>
+  );
+}
