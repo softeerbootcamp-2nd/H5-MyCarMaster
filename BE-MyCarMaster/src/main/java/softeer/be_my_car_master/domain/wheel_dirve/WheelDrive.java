05589226da58253bf3c63d46f829854c2f2144a3
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2023-08-09 17:42:00 +0900

    [Feature] 구동방식 목록 조회 구현 (#147)
    * feat: 구동방식 조회 API Spec 변경
    
    구동방식 목록 조회시 trimId, engineId로 식별
    
    * feat: WheelDrive domain 추가
    
    * feat: WheelDrive entity 추가
    
    * feat: WheelDrive 관련 중간테이블 entity 추가
    
    * feat: WheelDrivePort 추가
    
    * feat: WheelDrive 목록 조회 관련 JpaRepository 구현
    
    * feat: WheelDrive JpaAdapter 구현
    
    * feat: 구동방식 조회 API Spec 수정에 따른 TC 수정
    
    * feat: WheelDrive domain, dto 매핑을 위한 from 메소드 구현
    
    * feat: 트림, 엔진에 따라 선택가능한 구동방식 목록 조회 구현
    
    * feat: GetWheelDrivesUseCase TC 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/domain/wheel_dirve/WheelDrive.java
@@ -0,0 +1,26 @@
+package softeer.be_my_car_master.domain.wheel_dirve;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import softeer.be_my_car_master.global.annotation.Domain;
+
+@Domain
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class WheelDrive {
+	private Long id;
+	private String name;
+	private String description;
+	private Integer ratio;
+	private Integer price;
+	private String imgUrl;
+
+	public boolean isSelectable(List<Long> unselectableWheelDriveIds) {
+		return !unselectableWheelDriveIds.contains(id);
+	}
+}
