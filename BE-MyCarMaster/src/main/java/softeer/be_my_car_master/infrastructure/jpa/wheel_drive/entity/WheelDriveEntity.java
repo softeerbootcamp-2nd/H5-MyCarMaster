86bced4d6c5a98b2926961316b929c09d4c2277f
imdh <eastlake520@gmail.com>
2023-08-09 22:26:11 +0900

    feat: 중간 테이블 생성을 위한 임시 Entity 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/wheel_drive/entity/WheelDriveEntity.java
@@ -0,0 +1,36 @@
+package softeer.be_my_car_master.infrastructure.jpa.wheel_drive.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "wheel_drive")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WheelDriveEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	@Column(name = "ratio", nullable = false)
+	private Integer ratio;
+
+	@Column(name = "price", nullable = false)
+	private Integer price;
+
+	@Column(name = "img_url", nullable = false)
+	private String imgUrl;
+}
