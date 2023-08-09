a17c9fdf90657e580f1e299b78a09264f29f08a3
imdh <eastlake520@gmail.com>
2023-08-10 00:28:35 +0900

    chore: EngineEntity에 @Entity 추가

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/engine/entity/EngineEntity.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/engine/entity/EngineEntity.java
@@ -15,6 +15,7 @@ import lombok.NoArgsConstructor;
 import softeer.be_my_car_master.domain.engine.Engine;
 import softeer.be_my_car_master.infrastructure.jpa.model.entity.ModelEntity;
 
+@Entity
 @Table(name = "engine")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EngineEntity {
