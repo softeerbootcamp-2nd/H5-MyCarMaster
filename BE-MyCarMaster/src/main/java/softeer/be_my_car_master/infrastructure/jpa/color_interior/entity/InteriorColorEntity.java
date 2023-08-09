cfc1dc1e971ae5440143965eeafb601190921333
imdh <eastlake520@gmail.com>
2023-08-10 01:00:54 +0900

    feat: 색상 Entity에 model과의 관계 추가

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/color_interior/entity/InteriorColorEntity.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/color_interior/entity/InteriorColorEntity.java
@@ -2,9 +2,12 @@ package softeer.be_my_car_master.infrastructure.jpa.color_interior.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AccessLevel;
@@ -12,6 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import softeer.be_my_car_master.domain.color_exterior.ExteriorColor;
 import softeer.be_my_car_master.domain.color_interior.InteriorColor;
+import softeer.be_my_car_master.infrastructure.jpa.model.entity.ModelEntity;
 
 @Entity
 @Table(name = "interior_color")
@@ -28,6 +32,10 @@ public class InteriorColorEntity {
 
 	@Column(name = "color_img_url", nullable = false)
 	private String colorImgUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "model_id")
+	private ModelEntity model;
 
 	public InteriorColor toInteriorColor() {
 		return InteriorColor.builder()
