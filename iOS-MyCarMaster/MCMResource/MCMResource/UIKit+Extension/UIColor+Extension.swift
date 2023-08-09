a9cc49d378781fe80fbcb9e20c003a2f150ec325
miniOS <seungmin.oh.dev@gmail.com>
2023-08-09 23:49:31 +0900

    chore: 선언된 색을 UIColor에서 사용할 수 있도록 Namespace 선언

---
+++ iOS-MyCarMaster/MCMResource/MCMResource/UIKit+Extension/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  MCMResource
+//
+//  Created by SEUNGMIN OH on 2023/08/09.
+//
+
+import UIKit
+
+extension UIColor {
+    public typealias MCM = Resource.Color
+}
