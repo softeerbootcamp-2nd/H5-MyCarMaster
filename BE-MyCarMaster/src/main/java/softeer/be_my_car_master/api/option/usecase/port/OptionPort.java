66dfaa32009b16ae1a79be4175e90e65c39af5d5
imdh <eastlake520@gmail.com>
2023-08-09 22:28:32 +0900

    feat: OptionPort, TagPort 추가
    OptionPort에는 관련 추상 메서드 추가하고, TagPort는 새롭게 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/option/usecase/port/OptionPort.java
@@ -0,0 +1,20 @@
+package softeer.be_my_car_master.api.option.usecase.port;
+
+import java.util.List;
+
+import softeer.be_my_car_master.domain.option.Option;
+import softeer.be_my_car_master.global.annotation.Port;
+
+@Port
+public interface OptionPort {
+
+	List<Option> findSelectableOptionsByTrimId(Long trimId);
+
+	List<Long> findUnselectableOptionIdsByEngineId(Long engineId);
+
+	List<Long> findUnselectableOptionIdsByWheelDriveId(Long wheelDriveId);
+
+	List<Long> findUnselectableOptionIdsByBodyTypeId(Long bodyTypeId);
+
+	List<Long> findUnselectableOptionIdsByInteriorColorId(Long interiorColorId);
+}
