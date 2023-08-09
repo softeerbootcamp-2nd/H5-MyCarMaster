4cfdc0c80cdf42713a25ceb663bf2d06d7313787
imdh <eastlake520@gmail.com>
2023-08-09 22:24:25 +0900

    feat: 도메인 클래스 추가
    Option, Tag 등 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/domain/option/Option.java
@@ -0,0 +1,33 @@
+package softeer.be_my_car_master.domain.option;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import softeer.be_my_car_master.global.annotation.Domain;
+
+@Domain
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Option {
+
+	private Long id;
+	private String name;
+	private String category;
+	private String summary;
+	private String description;
+	private String imgUrl;
+	private Integer price;
+	private Integer ratio;
+	private Boolean isSuper;
+	// private List<Option> superOptions;
+	private List<Option> subOptions;
+	private Tag tag;
+
+	public String getTagName() {
+		return tag.getName();
+	}
+}
