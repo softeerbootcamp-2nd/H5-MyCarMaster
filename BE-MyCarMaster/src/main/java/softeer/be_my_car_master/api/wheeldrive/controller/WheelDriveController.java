f57fdf9a4570aa753a86b2773362ab9aa4fa8e22
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2023-08-10 20:14:31 +0900

    [Fix] 구동방식 목록 조회 요청 형식 query param으로 변경 (#164)
    * feat: 구동방식 목록 조회 요청 형식 query param으로 수정
    
    json을 body 받는 형태에서 query param을 받는 것으로 변경
    
    * test: WheelDriveController TC 수정
    
    * chore: WheelDriveController TC의 사용되지 않는 변수, 메소드 제거
    
    * chore: 컨벤션 맞추기 위해 인자 한 줄 띄움

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/wheeldrive/controller/WheelDriveController.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/wheeldrive/controller/WheelDriveController.java
@@ -2,8 +2,8 @@ package softeer.be_my_car_master.api.wheeldrive.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import softeer.be_my_car_master.api.wheeldrive.dto.request.GetWheelDrivesRequest;
 import softeer.be_my_car_master.api.wheeldrive.dto.response.GetWheelDrivesResponse;
 import softeer.be_my_car_master.api.wheeldrive.usecase.GetWheelDrivesUseCase;
+import softeer.be_my_car_master.global.exception.BindingParamException;
 import softeer.be_my_car_master.global.response.Response;
 
 @RestController
@@ -24,8 +25,13 @@ public class WheelDriveController {
 	@GetMapping("/wheel-drives")
 	@Operation(summary = "트림, 엔진에 따른 구동 방식 목록을 반환합니다")
 	public Response<GetWheelDrivesResponse> getWheelDrives(
-		@RequestBody @Valid GetWheelDrivesRequest getWheelDrivesRequest
+		@Valid GetWheelDrivesRequest getWheelDrivesRequest,
+		BindingResult bindingResult
 	) {
+		if (bindingResult.hasErrors()) {
+			throw new BindingParamException(bindingResult.getFieldErrors());
+		}
+
 		Long trimId = getWheelDrivesRequest.getTrimId();
 		Long engineId = getWheelDrivesRequest.getEngineId();
 		GetWheelDrivesResponse getWheelDrivesResponse = getWheelDrivesUseCase.execute(trimId, engineId);
