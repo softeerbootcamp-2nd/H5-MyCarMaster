b465bc1901978b9c1a4f49503a2754fe78106f41
imdh <eastlake520@gmail.com>
2023-08-10 09:00:13 +0900

    [Setting] 테이블명 설정 (#152)
    * feat: Entity에서 테이블명 설정
    
    * chore: 테이블명 수정

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/color_interior/entity/UnselectableExteriorInteriorEntity.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/color_interior/entity/UnselectableExteriorInteriorEntity.java
@@ -7,10 +7,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import softeer.be_my_car_master.infrastructure.jpa.color_exterior.entity.ExteriorColorEntity;
 
 @Entity
+@Table(name = "trim_exterior_interior")
 public class UnselectableExteriorInteriorEntity {
 
 	@Id
--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/wheel_drive/entity/UnselectableEngineWheelDriveEntity.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/wheel_drive/entity/UnselectableEngineWheelDriveEntity.java
@@ -7,10 +7,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import softeer.be_my_car_master.infrastructure.jpa.engine.entity.EngineEntity;
 
 @Entity
+@Table(name = "unselectable_engine_wheel_drive")
 public class UnselectableEngineWheelDriveEntity {
 
 	@Id
