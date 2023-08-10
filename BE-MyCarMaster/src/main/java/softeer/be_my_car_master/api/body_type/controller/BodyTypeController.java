cd700d7a4e61d9d74f6d1f818fd090de652928ae
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-10 19:27:24 +0900

    feat: 구동방식 목록 조회 요청 형식 query param으로 수정
    json을 body 받는 형태에서 query param을 받는 것으로 변경

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/body_type/controller/BodyTypeController.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/body_type/controller/BodyTypeController.java
@@ -2,8 +2,8 @@ package softeer.be_my_car_master.api.body_type.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import softeer.be_my_car_master.api.body_type.dto.request.GetBodyTypesRequest;
 import softeer.be_my_car_master.api.body_type.dto.response.GetBodyTypesResponse;
 import softeer.be_my_car_master.api.body_type.usecase.GetBodyTypesUseCase;
+import softeer.be_my_car_master.global.exception.BindingParamException;
 import softeer.be_my_car_master.global.response.Response;
 
 @RestController
@@ -23,7 +24,12 @@ public class BodyTypeController {
 
 	@GetMapping("/body-types")
 	@Operation(summary = "모델에 따른 바디타입 목록을 반환합니다")
-	public Response<GetBodyTypesResponse> getBodyTypes(@RequestBody @Valid GetBodyTypesRequest getBodytypesRequest) {
+	public Response<GetBodyTypesResponse> getBodyTypes(@Valid GetBodyTypesRequest getBodytypesRequest,
+		BindingResult bindingResult
+	) {
+		if (bindingResult.hasErrors()) {
+			throw new BindingParamException(bindingResult.getFieldErrors());
+		}
 
 		Long modelId = getBodytypesRequest.getModelId();
 		GetBodyTypesResponse getBodyTypesResponse = getBodyTypesUseCase.execute(modelId);
