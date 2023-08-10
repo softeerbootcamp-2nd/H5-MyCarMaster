cc9424bf008baba42721e39c4d6820cae4ce0165
imdh <eastlake520@gmail.com>
2023-08-10 19:55:51 +0900

    feat: 외장 색상 목록 조회 요청 형식 수정

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/color_exterior/controller/ExteriorColorController.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/color_exterior/controller/ExteriorColorController.java
@@ -2,6 +2,7 @@ package softeer.be_my_car_master.api.color_exterior.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import softeer.be_my_car_master.api.color_exterior.dto.request.GetExteriorColorsRequest;
 import softeer.be_my_car_master.api.color_exterior.dto.response.GetExteriorColorsResponse;
 import softeer.be_my_car_master.api.color_exterior.usecase.GetExteriorColorsUseCase;
+import softeer.be_my_car_master.global.exception.BindingParamException;
 import softeer.be_my_car_master.global.response.Response;
 
 @RestController
@@ -24,7 +26,13 @@ public class ExteriorColorController {
 	@GetMapping("/exterior-colors")
 	@Operation(summary = "트림에서 선택가능한 외장 색상 목록을 반환합니다")
 	public Response<GetExteriorColorsResponse> getExteriorColors(
-		@RequestBody @Valid GetExteriorColorsRequest getExteriorColorsRequest) {
+		@Valid GetExteriorColorsRequest getExteriorColorsRequest,
+		BindingResult bindingResult
+	) {
+		if (bindingResult.hasErrors()) {
+			throw new BindingParamException(bindingResult.getFieldErrors());
+		}
+
 		Long trimId = getExteriorColorsRequest.getTrimId();
 		GetExteriorColorsResponse getExteriorColorsResponse = getExteriorColorsUseCase.execute(trimId);
 		return Response.createSuccessResponse(getExteriorColorsResponse);
