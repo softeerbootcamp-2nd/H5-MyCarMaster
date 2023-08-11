b52b873b893332afca88667b3c5b70c780f2c30e
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-11 22:17:48 +0900

    feat: 선택된 옵션 중 엔진에서 선택불가능한 옵션 조회 구현
    '트림에서 선택가능한 옵션 목록' 중 '선택된 옵션'이면서 '엔진에서 선택 불가능한 옵션' 필터링
    -> '선택된 옵션' 중, '해당 트림에서 선택불가능한 옵션' 예외처리 후 '엔진에서 선택 불가능한 옵션' 필터링 하도록 수정

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/engine/usecase/GetUnselectableOptionsByEngineUseCase.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/engine/usecase/GetUnselectableOptionsByEngineUseCase.java
@@ -8,6 +8,7 @@ import softeer.be_my_car_master.api.engine.dto.response.GetUnselectableOptionsBy
 import softeer.be_my_car_master.api.option.usecase.port.OptionPort;
 import softeer.be_my_car_master.domain.option.Option;
 import softeer.be_my_car_master.global.annotation.UseCase;
+import softeer.be_my_car_master.global.exception.InvalidOptionIdException;
 
 @UseCase
 @RequiredArgsConstructor
@@ -19,15 +20,46 @@ public class GetUnselectableOptionsByEngineUseCase {
 		List<Option> selectableOptions = optionPort.findSelectableOptionsByTrimId(trimId);
 		List<Long> unselectableOptionIdsByEngine = optionPort.findUnselectableOptionIdsByEngineId(engineId);
 
-		List<Option> unselectableOptionsByEngine = selectableOptions.stream()
-			.filter(option -> isUnselectableOptionByEngine(option, selectedOptionIds, unselectableOptionIdsByEngine))
-			.collect(Collectors.toList());
-
+		List<Option> unselectableOptionsByEngine = filterUnselectableOptionsByEngine(
+			selectedOptionIds,
+			selectableOptions,
+			unselectableOptionIdsByEngine
+		);
 		return GetUnselectableOptionsByEngineResponse.from(unselectableOptionsByEngine);
 	}
 
-	private static boolean isUnselectableOptionByEngine(Option option, List<Long> selectedOptionIds,
-		List<Long> unselectableOptionIdsByEngine) {
-		return selectedOptionIds.contains(option.getId()) && unselectableOptionIdsByEngine.contains(option.getId());
+	private List<Option> filterUnselectableOptionsByEngine(
+		List<Long> selectedOptionIds,
+		List<Option> selectableOptions,
+		List<Long> unselectableOptionIdsByEngine
+	) {
+		return selectedOptionIds.stream()
+			.filter(
+				optionId -> isUnselectableOptionByEngine(optionId, selectableOptions, unselectableOptionIdsByEngine)
+			)
+			.map(optionId -> findOptionById(selectableOptions, optionId))
+			.collect(Collectors.toList());
+	}
+
+	private static boolean isUnselectableOptionByEngine(
+		Long optionId,
+		List<Option> selectableOptions,
+		List<Long> unselectableOptionIdsByEngine
+	) {
+		if (!isValidOptionId(optionId, selectableOptions)) {
+			throw new InvalidOptionIdException();
+		}
+		return unselectableOptionIdsByEngine.contains(optionId);
+	}
+
+	private static boolean isValidOptionId(Long optionId, List<Option> selectableOptions) {
+		return selectableOptions.stream().anyMatch(option -> option.getId().equals(optionId));
+	}
+
+	private static Option findOptionById(List<Option> selectableOptions, Long optionId) {
+		return selectableOptions.stream()
+			.filter(option -> option.getId().equals(optionId))
+			.findFirst()
+			.orElseThrow(InvalidOptionIdException::new);
 	}
 }
