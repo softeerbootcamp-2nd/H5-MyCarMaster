2210336dd223d5681d6ae6fadccc046380026215
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-11 22:22:21 +0900

    test: GetUnselectableOptionsByEngineUseCase TC 수정 및 예외처리 TC 추가
    OptionIds가 트림에서 선택 불가능한 옵션을 포함할 때, InvalidOptionIdException이 발생하는지 테스트

--- BE-MyCarMaster/src/test/java/softeer/be_my_car_master/api/engine/usecase/GetUnselectableOptionsByEngineUseCaseTest.java
+++ BE-MyCarMaster/src/test/java/softeer/be_my_car_master/api/engine/usecase/GetUnselectableOptionsByEngineUseCaseTest.java
@@ -1,5 +1,6 @@
 package softeer.be_my_car_master.api.engine.usecase;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 
@@ -18,6 +19,7 @@ import softeer.be_my_car_master.api.engine.dto.response.GetUnselectableOptionsBy
 import softeer.be_my_car_master.api.engine.dto.response.UnselectableOptionDto;
 import softeer.be_my_car_master.api.option.usecase.port.OptionPort;
 import softeer.be_my_car_master.domain.option.Option;
+import softeer.be_my_car_master.global.exception.InvalidOptionIdException;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("GetUnselectableOptionsByEngineUseCase Test")
@@ -33,7 +35,7 @@ class GetUnselectableOptionsByEngineUseCaseTest {
 	@DisplayName("엔진에 따라 선택 불가능한 옵션 목록을 조회합니다")
 	void execute() {
 		// given
-		Option option = Option.builder()
+		Option option1 = Option.builder()
 			.id(1L)
 			.name("임의의 옵션")
 			.category("SAFE")
@@ -47,7 +49,21 @@ class GetUnselectableOptionsByEngineUseCaseTest {
 			.tag(null)
 			.build();
 
-		given(optionPort.findSelectableOptionsByTrimId(any())).willReturn(Arrays.asList(option));
+		Option option2 = Option.builder()
+			.id(2L)
+			.name("임의의 옵션2")
+			.category("SAFE")
+			.summary("옵션 요약")
+			.description("옵션 상세설명")
+			.imgUrl("imgUrl")
+			.price(100000)
+			.ratio(22)
+			.isSuper(false)
+			.subOptions(null)
+			.tag(null)
+			.build();
+
+		given(optionPort.findSelectableOptionsByTrimId(any())).willReturn(Arrays.asList(option1, option2));
 		given(optionPort.findUnselectableOptionIdsByEngineId(any())).willReturn(Arrays.asList(1L, 3L));
 
 		// when
@@ -61,9 +77,38 @@ class GetUnselectableOptionsByEngineUseCaseTest {
 		SoftAssertions.assertSoftly(softAssertions -> {
 			softAssertions.assertThat(options).isNotNull();
 			softAssertions.assertThat(options).hasSize(1);
-			softAssertions.assertThat(optionExpected.getId()).isEqualTo(option.getId());
-			softAssertions.assertThat(optionExpected.getName()).isEqualTo(option.getName());
-			softAssertions.assertThat(optionExpected.getPrice()).isEqualTo(option.getPrice());
+			softAssertions.assertThat(optionExpected.getId()).isEqualTo(option1.getId());
+			softAssertions.assertThat(optionExpected.getName()).isEqualTo(option1.getName());
+			softAssertions.assertThat(optionExpected.getPrice()).isEqualTo(option1.getPrice());
 		});
+	}
+
+	@Test
+	@DisplayName("OptionIds가 트림에서 선택 불가능한 옵션을 포함한다면 InvalidOptionIdException이 발생합니다")
+	void invalidOptionIds() {
+		// given
+		Option option1 = Option.builder()
+			.id(1L)
+			.name("임의의 옵션")
+			.category("SAFE")
+			.summary("옵션 요약")
+			.description("옵션 상세설명")
+			.imgUrl("imgUrl")
+			.price(100000)
+			.ratio(22)
+			.isSuper(false)
+			.subOptions(null)
+			.tag(null)
+			.build();
+
+		given(optionPort.findSelectableOptionsByTrimId(any())).willReturn(Arrays.asList(option1));
+		given(optionPort.findUnselectableOptionIdsByEngineId(any())).willReturn(Arrays.asList(1L, 3L));
+
+		// when
+		// then
+		assertThrows(
+			InvalidOptionIdException.class,
+			() -> getUnselectableOptionsByEngineUseCase.execute(1L, 1L, Arrays.asList(1L, 2L))
+		);
 	}
 }
