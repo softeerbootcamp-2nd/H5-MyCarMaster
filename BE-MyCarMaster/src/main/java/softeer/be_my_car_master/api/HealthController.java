8ca50e48e400087ce4ca5832aac5079a50718a3e
jangdongho <ehdgh25@naver.com>
2023-08-11 09:46:38 +0900

    feat: server 시간 health 체크시 확인

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/HealthController.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/HealthController.java
@@ -2,6 +2,7 @@ package softeer.be_my_car_master.api;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class HealthController {
 		try {
 			InetAddress ip = InetAddress.getLocalHost();
 			String hostname = ip.getHostName();
-			return "Your current IP address : " + ip + ", Hostname : " + hostname;
+			return "Your current IP address : " + ip + ", Hostname : " + hostname + ", server time : " + LocalDateTime.now();
 		} catch (UnknownHostException e) {
 			return "Error retrieving IP and Hostname";
 		}
