6fe0fa07692630c7c5bf044ce31c1660c6347708
imdh <eastlake520@gmail.com>
2023-08-11 09:58:24 +0900

    [Fix] checkStyle 수정 (#187)

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/HealthController.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/HealthController.java
@@ -15,7 +15,9 @@ public class HealthController {
 		try {
 			InetAddress ip = InetAddress.getLocalHost();
 			String hostname = ip.getHostName();
-			return "Your current IP address : " + ip + ", Hostname : " + hostname + ", server time : " + LocalDateTime.now();
+			return "Your current IP address : " + ip
+				+ ", Hostname : " + hostname
+				+ ", server time : " + LocalDateTime.now();
 		} catch (UnknownHostException e) {
 			return "Error retrieving IP and Hostname";
 		}
