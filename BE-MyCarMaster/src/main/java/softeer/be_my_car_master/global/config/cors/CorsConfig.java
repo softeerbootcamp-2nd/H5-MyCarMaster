c5219c630d356cc6377a439cc2bae1d26abd0129
jangdongho <ehdgh25@naver.com>
2023-08-11 09:37:51 +0900

    setting: CORS 설정
    Vercel 기본 도메인에서 구매한 도메인 기준으로 CORS 설정

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/global/config/cors/CorsConfig.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/global/config/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://h5-my-car-master.vercel.app", "http://localhost:5173")
+			.allowedOrigins("https://my-car-master.shop", "https://*.my-car-master.shop", "http://localhost:5173")
 			.allowedMethods("GET", "POST")
 			.maxAge(3000);
 	}
