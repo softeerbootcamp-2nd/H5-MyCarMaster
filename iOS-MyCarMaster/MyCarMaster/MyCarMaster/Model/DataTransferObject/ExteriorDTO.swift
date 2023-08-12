5d038d4436863a7aa8de216dbc46c83024c1faa4
miniOS <seungmin.oh.dev@gmail.com>
2023-08-13 04:05:12 +0900

    feat: Exterior Entity, DTO 구현

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Model/DataTransferObject/ExteriorDTO.swift
@@ -0,0 +1,37 @@
+//
+//  ExteriorDTO.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/12.
+//
+
+import Foundation
+
+struct ExteriorDTO: Codable {
+    let id: Int
+    let name: String
+    let price: Int
+    let ratio: Int
+    let colorImgURL: String
+    let coloredImgURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case price = "price"
+        case ratio = "ratio"
+        case colorImgURL = "colorImgUrl"
+        case coloredImgURL = "coloredImgUrl"
+    }
+}
+
+extension Exterior {
+    init(_ exteriorDTO: ExteriorDTO) {
+        self.model = "펠리세이드"
+        self.name = exteriorDTO.name
+        self.price = exteriorDTO.price
+        self.ratio = exteriorDTO.ratio
+        self.colorImgURL = URL(string: exteriorDTO.colorImgURL)
+        self.coloredImgURL = URL(string: exteriorDTO.coloredImgURL)
+    }
+}
---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Model/Entity/Exterior.swift
@@ -0,0 +1,17 @@
+//
+//  Exterior.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/12.
+//
+
+import Foundation
+
+struct Exterior {
+    let model: String
+    let name: String
+    let price: Int
+    let ratio: Int
+    let colorImgURL: URL?
+    let coloredImgURL: URL?
+}
