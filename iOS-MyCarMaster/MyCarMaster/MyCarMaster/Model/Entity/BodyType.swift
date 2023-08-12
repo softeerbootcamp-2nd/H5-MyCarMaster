cabb4c5f2eb19a2cbcbfdb736961a93a17ca99af
miniOS <seungmin.oh.dev@gmail.com>
2023-08-12 16:46:20 +0900

    feat: BodyType Entity, DTO 구현

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Model/Entity/BodyType.swift
@@ -0,0 +1,17 @@
+//
+//  BodyType.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/12.
+//
+
+import Foundation
+
+struct BodyType {
+    var model: String
+    var name: String
+    var description: String
+    var ratio: Int
+    var price: Int
+    var imageURL: URL?
+}
