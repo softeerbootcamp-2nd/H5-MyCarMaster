1a4c9f17d24326ffb30894549d7115885903f74e
miniOS <seungmin.oh.dev@gmail.com>
2023-08-12 16:07:38 +0900

    feat: Engine(Entity), EngineDTO 추가

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Model/Entity/Engine.swift
@@ -0,0 +1,21 @@
+//
+//  Engine.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/12.
+//
+
+import Foundation
+
+struct Engine {
+    let model: String
+    let name: String
+    let ratio: Int
+    let description: String
+    let fuelMin: Double
+    let fuelMax: Double
+    let power: Int
+    let toque: Double
+    let price: Int
+    var imageURL: URL?
+}
