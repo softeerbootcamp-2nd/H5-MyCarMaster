d5399ee04e55b96de67b40ac01cd801bc91d0fcc
miniOS <seungmin.oh.dev@gmail.com>
2023-08-14 01:23:48 +0900

    feat: WheelDrive를 BasicCellState로 변환 가능하도록 Convertible 프로토콜 적용

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Model/Entity+/WheelDrive+.swift
@@ -0,0 +1,21 @@
+//
+//  WheelDrive+.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/14.
+//
+
+import Foundation
+
+extension WheelDrive: BasicListCellStateConvertible {
+    var basicListCellState: BasicListCellState {
+        return .init(
+            model: self.model,
+            name: self.name,
+            description: self.description,
+            ratio: self.ratio,
+            price: self.price,
+            hasDetailButton: false
+        )
+    }
+}
