82baf459eecc4ce3b4764f6462021046644498c5
miniOS <seungmin.oh.dev@gmail.com>
2023-08-14 01:25:20 +0900

    feat: BodyType을 BasicCellState로 변환 가능하도록 Convertible 프로토콜 적용

--- iOS-MyCarMaster/MyCarMaster/MyCarMaster/General/Cell/BasicListCellState.swift
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/General/Cell/BasicListCellState.swift
@@ -19,18 +19,3 @@ struct BasicListCellState {
 protocol BasicListCellStateConvertible {
     var basicListCellState: BasicListCellState { get }
 }
-
-extension BasicListCellState {
-    init(from trim: Trim) {
-        self
-    }
-    init(from engine: Engine) {
-        self.init(model: engine.model, name: engine.name, description: engine.description, ratio: engine.ratio, price: engine.price, hasDetailButton: false)
-    }
-    init(from wheelDrive: WheelDrive) {
-        self.init(model: wheelDrive.model, name: wheelDrive.name, description: wheelDrive.description, ratio: wheelDrive.ratio, price: wheelDrive.price, hasDetailButton: false)
-    }
-    init(from bodyType: BodyType) {
-        self.init(model: bodyType.model, name: bodyType.name, description: bodyType.description, ratio: bodyType.ratio, price: bodyType.price, hasDetailButton: false)
-    }
-}
