0b01e745903d4fbb8d0248e6c7abe2c0458a0e89
wonho1401 <wonho1401@naver.com>
2023-08-14 02:21:34 +0900

    feat: 옵션 총합 부분 추가

--- FE-MyCarMaster/src/components/common/QuotationList/QuotationOptionList.tsx
+++ FE-MyCarMaster/src/components/common/QuotationList/QuotationOptionList.tsx
@@ -9,6 +9,14 @@ function QuotationOptionList() {
   const { optionQuotation } = useQuotationState();
   console.log(optionQuotation);
 
+  const calculateOptionTotalPrice = () => {
+    const totalPrice = optionQuotation.selectedQuotation.reduce(
+      (acc, item) => acc + item.price,
+      0
+    );
+    return totalPrice;
+  };
+
   return (
     <Container>
       <CategoryContainer>
@@ -17,30 +25,40 @@ function QuotationOptionList() {
       </CategoryContainer>
       <ItemContainer>
         <TextButton size={"1.25"} text={"기본 포함 옵션"} />
-        <OptionContainer>
-          <Option>추가 옵션</Option>
-          {optionQuotation.selectedQuotation.map((option, index) => (
-            <QuotationOptionItem
-              key={index}
-              imgUrl={option.imgUrl as string}
-              category={option.category as string}
-              name={option.name}
-              price={option.price}
-              isSelected={true}
-            />
-          ))}
-          <Option>고민 옵션</Option>
-          {optionQuotation.consideredQuotation.map((option, index) => (
-            <QuotationOptionItem
-              key={index}
-              imgUrl={option.imgUrl as string}
-              category={option.category as string}
-              name={option.name}
-              price={option.price}
-              isSelected={false}
-            />
-          ))}
-        </OptionContainer>
+        <TotalOptionContainer>
+          <OptionContainer>
+            <Option>추가 옵션</Option>
+            {optionQuotation.selectedQuotation.map((option, index) => (
+              <QuotationOptionItem
+                key={index}
+                imgUrl={option.imgUrl as string}
+                category={option.category as string}
+                name={option.name}
+                price={option.price}
+                isSelected={true}
+              />
+            ))}
+          </OptionContainer>
+          <OptionContainer>
+            <Option>고민 옵션</Option>
+            {optionQuotation.consideredQuotation.map((option, index) => (
+              <QuotationOptionItem
+                key={index}
+                imgUrl={option.imgUrl as string}
+                category={option.category as string}
+                name={option.name}
+                price={option.price}
+                isSelected={false}
+              />
+            ))}
+          </OptionContainer>
+          <TotalPriceContainer>
+            <TotalText>총합</TotalText>
+            <TotalPrice>
+              +{calculateOptionTotalPrice().toLocaleString("ko-KR")}원
+            </TotalPrice>
+          </TotalPriceContainer>
+        </TotalOptionContainer>
       </ItemContainer>
     </Container>
   );
@@ -81,6 +99,13 @@ const ItemContainer = styled.div`
   align-items: flex-end;
 `;
 
+const TotalOptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: flex-start;
+  gap: 4rem;
+`;
+
 const OptionContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -89,6 +114,28 @@ const OptionContainer = styled.div`
 `;
 
 const Option = styled.div`
+  font-family: "HyundaiSansMedium";
+  font-size: 1.375rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.75rem;
+`;
+
+const TotalPriceContainer = styled.div`
+  width: 42.25rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TotalText = styled.p`
+  font-family: "HyundaiSansMedium";
+  font-size: 1.375rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.75rem;
+`;
+
+const TotalPrice = styled.p`
   font-family: "HyundaiSansMedium";
   font-size: 1.375rem;
   font-style: normal;
