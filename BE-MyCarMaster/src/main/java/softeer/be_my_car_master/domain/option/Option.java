8b13a253e2d4389fe23474e5a3060c8595699ac1
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-14 09:35:23 +0900

    chore: option usecase에서 공통으로 사용되는 메소드 위치 수정

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/domain/option/Option.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/domain/option/Option.java
@@ -1,9 +1,6 @@
 package softeer.be_my_car_master.domain.option;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -35,22 +32,5 @@ public class Option {
 
 	public Boolean hasSubOption() {
 		return isSuper;
-	}
-
-	public static Set<Long> combineUnselectableOptionIds(List<Long>... unselectableOptionIdsLists) {
-		Set<Long> combinedSet = new HashSet<>();
-		for (List<Long> list : unselectableOptionIdsLists) {
-			combinedSet.addAll(list);
-		}
-		return combinedSet;
-	}
-
-	public static List<Option> filterOptionsByUnselectableIds(
-		List<Option> selectableOptions,
-		Set<Long> unselectableOptionIdsSet
-	) {
-		return selectableOptions.stream()
-			.filter(option -> !unselectableOptionIdsSet.contains(option.getId()))
-			.collect(Collectors.toList());
 	}
 }
