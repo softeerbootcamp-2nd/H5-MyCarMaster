db121a1b9ba5db3572bdb3861116bf8da2f9c662
imdh <eastlake520@gmail.com>
2023-08-14 22:49:15 +0900

    chore: dangling import 제거

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/option/repository/OptionJpaRepository.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/option/repository/OptionJpaRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import softeer.be_my_car_master.infrastructure.jpa.option.entity.OptionEntity;
-import softeer.be_my_car_master.infrastructure.jpa.option.entity.TrimAdditionalOptionEntity;
 
 public interface OptionJpaRepository extends JpaRepository<OptionEntity, Long> {
 
