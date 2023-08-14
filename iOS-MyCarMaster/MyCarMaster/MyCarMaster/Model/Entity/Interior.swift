43a722b74d2e2c28951cc536440a04d4ac5a9beb
miniOS <seungmin.oh.dev@gmail.com>
2023-08-14 11:27:06 +0900

    feat: Interior Entity 선언

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Model/Entity/Interior.swift
@@ -0,0 +1,17 @@
+//
+//  Interior.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/14.
+//
+
+import Foundation
+
+struct Interior {
+    let model: String
+    let name: String
+    let price: Int
+    let ratio: Int
+    let colorImgURL: URL?
+    let coloredImgURL: URL?
+}
