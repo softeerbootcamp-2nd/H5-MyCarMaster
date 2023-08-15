32d72fb0f3a6a3c9ecd51517e740f9ac0202b2eb
miniOS <seungmin.oh.dev@gmail.com>
2023-08-16 01:50:18 +0900

    fix: Cell + Selectable에서 테두리 두께 지정

--- iOS-MyCarMaster/MyCarMaster/MyCarMaster/General/Selectable/UICollectionViewCell+Selectable.swift
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/General/Selectable/UICollectionViewCell+Selectable.swift
@@ -10,11 +10,13 @@ import UIKit
 extension Selectable where Self: UICollectionViewCell {
     func selectedStyle() {
         backgroundColor = selectedBackgroundColor
+        layer.borderWidth = 1.0
         layer.borderColor = selectedBorderColor
     }
     
     func unselectedStyle() {
         backgroundColor = unselectedBackgroundColor
+        layer.borderWidth = 1.0
         layer.borderColor = unselectedBorderColor
     }
 }
