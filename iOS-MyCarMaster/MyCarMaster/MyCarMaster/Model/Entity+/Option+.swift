f81e59b96c55adb278d2ed05e6096971a05efbf8
miniOS <seungmin.oh.dev@gmail.com>
2023-08-16 05:06:23 +0900

    feat: OptionListCellMainState 구현 및 Option Entity에 Convertible 적용

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Model/Entity+/Option+.swift
@@ -0,0 +1,20 @@
+//
+//  Option+.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/16.
+//
+
+import Foundation
+
+extension Option: OptionListCellMainStateConvertible {
+    var optionListCellMainState: OptionListCellMainState {
+        return .init(
+            model: self.model,
+            category: self.category,
+            name: self.name,
+            price: self.price,
+            ratio: self.price
+        )
+    }
+}
