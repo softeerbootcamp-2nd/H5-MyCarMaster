c262ce7047dc589dda0361876b05812342dd1feb
jangdongho <ehdgh25@naver.com>
2023-08-16 18:49:47 +0900

    feat: EnumValidator 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/global/annotation/valid/ValidEnum.java
@@ -0,0 +1,22 @@
+package softeer.be_my_car_master.global.annotation.valid;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = {EnumValidator.class})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+
+	String message() default "Invalid value. This is not permitted.";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	Class<? extends Enum<?>> enumClass();
+}
