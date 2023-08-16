f3a96f1c5b1efb30b2838c3b01234c2e14aebc3b
imdh <eastlake520@gmail.com>
2023-08-17 00:43:44 +0900

    feat: GetCarMasterUseCase 구현

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/car_master/usecase/GetCarMasterUseCase.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/car_master/usecase/GetCarMasterUseCase.java
@@ -1,13 +1,21 @@
 package softeer.be_my_car_master.api.car_master.usecase;
 
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 import softeer.be_my_car_master.api.car_master.dto.response.GetCarMasterResponse;
+import softeer.be_my_car_master.api.consult.usecase.port.CarMasterPort;
+import softeer.be_my_car_master.domain.car_master.CarMaster;
 import softeer.be_my_car_master.global.annotation.UseCase;
 
 @UseCase
+@RequiredArgsConstructor
 public class GetCarMasterUseCase {
 
-	public GetCarMasterResponse execute(Double latitude, Double longitude, String filter) {
+	private final CarMasterPort carMasterPort;
 
-		return null;
+	public GetCarMasterResponse execute(Double latitude, Double longitude, String filter) {
+		List<CarMaster> carMasters = carMasterPort.findCarMasters(latitude, longitude, filter);
+		return GetCarMasterResponse.from(carMasters);
 	}
 }
