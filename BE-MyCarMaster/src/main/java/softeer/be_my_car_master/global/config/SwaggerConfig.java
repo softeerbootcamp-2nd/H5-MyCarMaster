8bd8ffed1acf756e0f12f7f0b8902f8890a0286a
imdh <eastlake520@gmail.com>
2023-08-17 18:10:58 +0900

    [Setting] swagger server Url 설정 (#265)
    - api 서버 테스트 가능하도록 swagger server url 설정

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/global/config/SwaggerConfig.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/global/config/SwaggerConfig.java
@@ -1,22 +1,29 @@
 package softeer.be_my_car_master.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
+
+	@Value("${api.server.url}")
+	private String serverUrl;
 
 	@Bean
 	public OpenAPI openApi() {
 		Info info = new Info()
 			.title("My Car Master API Document")
-			.version("v0.0.1")
+			.version("v2.0.0")
 			.description("My Car Master의 API 명세서입니다.");
+
 		return new OpenAPI()
+			.addServersItem(new Server().url(serverUrl).description("Server URL"))
 			.components(new Components())
 			.info(info);
 	}
