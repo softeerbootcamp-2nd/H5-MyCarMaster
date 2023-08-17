6afd0e6748f8fbea3457070e99638c9af7c6f4a8
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-17 14:47:06 +0900

    feat: dto의 category type string으로 수정 후 Category.name을 응답하게 수정

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/option/dto/response/DefaultOptionDto.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/option/dto/response/DefaultOptionDto.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import softeer.be_my_car_master.domain.option.Category;
 import softeer.be_my_car_master.domain.option.Option;
 
 @Getter
@@ -18,8 +17,8 @@ public class DefaultOptionDto {
 	@Schema(description = "옵션 식별자", example = "1")
 	private Long id;
 
-	@Schema(description = "카테고리", example = "SAFE")
-	private Category category;
+	@Schema(description = "카테고리", example = "안전")
+	private String category;
 
 	@Schema(description = "옵션명", example = "주차보조 시스템||")
 	private String name;
@@ -33,7 +32,7 @@ public class DefaultOptionDto {
 	public static DefaultOptionDto from(Option option) {
 		return DefaultOptionDto.builder()
 			.id(option.getId())
-			.category(option.getCategory())
+			.category(option.getCategoryName())
 			.name(option.getName())
 			.imgUrl(option.getImgUrl())
 			.description(option.getDescription())
