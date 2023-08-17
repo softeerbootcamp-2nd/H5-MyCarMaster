04b3cdda53fff13776b263dc1a9ef3b0e866b52a
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-17 22:39:58 +0900

    feat: GetTrimDefaultOptionsUseCase 구현

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/trim/usecase/GetTrimDefaultOptionsUseCase.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/trim/usecase/GetTrimDefaultOptionsUseCase.java
@@ -1,14 +1,21 @@
 package softeer.be_my_car_master.api.trim.usecase;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
+import softeer.be_my_car_master.api.option.usecase.port.OptionPort;
 import softeer.be_my_car_master.api.trim.dto.response.GetTrimDefaultOptionsResponse;
+import softeer.be_my_car_master.domain.option.Option;
 import softeer.be_my_car_master.global.annotation.UseCase;
 
 @UseCase
 @RequiredArgsConstructor
 public class GetTrimDefaultOptionsUseCase {
 
+	private final OptionPort optionPort;
+
 	public GetTrimDefaultOptionsResponse execute(Long trimId) {
-		return null;
+		List<Option> defaultOptions = optionPort.findDefaultOptionsByTrimId(trimId);
+		return GetTrimDefaultOptionsResponse.from(defaultOptions);
 	}
 }
