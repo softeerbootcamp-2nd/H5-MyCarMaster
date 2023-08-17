850aac54b71b8e7be2091e17746ca81fbc988a92
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-17 17:33:41 +0900

    feat: TrimDefaultOptionDto 구현

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/trim/dto/response/TrimDefaultOptionDto.java
@@ -0,0 +1,45 @@
+package softeer.be_my_car_master.api.trim.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import softeer.be_my_car_master.domain.option.Option;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TrimDefaultOptionDto {
+
+	@Schema(description = "옵션 식별자", example = "1")
+	private Long id;
+
+	@Schema(description = "카테고리", example = "안전")
+	private String category;
+
+	@Schema(description = "옵션명", example = "주차보조 시스템||")
+	private String name;
+
+	@Schema(description = "옵션 이미지", example = "s3 url")
+	private String imgUrl;
+
+	@Schema(description = "옵션 설명", example = "null")
+	private String description;
+
+	@Schema(description = "해당 옵션 제공에 필요한 세부모델명", example = "null")
+	private String need;
+
+	public static TrimDefaultOptionDto from(Option option, String need) {
+		return TrimDefaultOptionDto.builder()
+			.id(option.getId())
+			.category(option.getCategoryValue())
+			.name(option.getName())
+			.imgUrl(option.getImgUrl())
+			.description(option.getDescription())
+			.need(need)
+			.build();
+	}
+}
