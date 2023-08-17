dea470294b831fe7b5e61bcda454ab4ade294c64
ParkByunggwon <startartart@naver.com>
2023-08-18 03:14:03 +0900

    feat: OptionContent 더미데이터 삭제 및 API 연결

--- FE-MyCarMaster/src/components/Main/view/OptionContent.tsx
+++ FE-MyCarMaster/src/components/Main/view/OptionContent.tsx
@@ -1,23 +1,61 @@
 import { styled } from "styled-components";
 import CategoryList from "../../common/CategoryList/CategoryList";
-import optionImg from "../../../assets/images/option.png";
 import OptionDescription from "../../common/OptionDescription/OptionDescription";
 import {
   useOptionDispatch,
   useOptionState,
 } from "../../../contexts/OptionContext";
-import { OptionState } from "../../../types/options.types";
+import useFetch from "../../../hooks/useFetch";
+import { useEffect } from "react";
+import { OptionType, OptionState } from "../../../types/options.types";
 import filterOptionCategory from "../../../utils/Option/filterOptionCategory";
 import { categories } from "../../../constants/Option.constants";
+import { useTrimState } from "../../../contexts/TrimContext";
+import { useDetailState } from "../../../contexts/DetailContext";
+import { useCarPaintState } from "../../../contexts/CarPaintContext";
 
+interface FetchOptionsProps extends OptionType {
+  result: {
+    options: OptionType[];
+  };
+}
 function OptionContent() {
   const { optionList, optionId }: OptionState = useOptionState();
   const optionDispatch = useOptionDispatch();
 
+  const { trimId } = useTrimState();
+  const { engineId, wheelDriveId, bodyTypeId } = useDetailState();
+  const { interiorId } = useCarPaintState();
+
+  const SERVER_URL = import.meta.env.VITE_APP_SERVER_URL;
+
+  const { data } = useFetch<FetchOptionsProps>(
+    `${SERVER_URL}/options?trimId=${trimId}&engineId=${engineId}&wheelDriveId=${wheelDriveId}&bodyTypeId=${bodyTypeId}&interiorColorId=${interiorId}`,
+    {
+      method: "GET",
+    }
+  );
+
+  useEffect(() => {
+    if (data) {
+      optionDispatch({
+        type: "SET_OPTION_LIST",
+        payload: { optionList: data.result.options },
+      });
+
+      optionDispatch({
+        type: "SET_OPTION_CATEGORY_INDEX",
+        payload: {
+          optionCategoryId: 0,
+          optionId: data.result.options[0].id,
+        },
+      });
+    }
+  }, [data, optionDispatch]);
+
   const onClickHandler = (index: number) => {
     const nextOptionCategoryId = index;
 
-    // 카테고리 이동시 첫 아이템으로 포커스 시키기 위한 리스트
     const filteredList = filterOptionCategory(
       categories,
       nextOptionCategoryId,
@@ -34,18 +72,25 @@ function OptionContent() {
   };
 
   return (
-    <Container>
-      <OptionContainer>
-        <OptionImg src={optionImg} />
-        {/* API 완성 시, optionList[optionId].imgUrl 로 교체*/}
-        <OptionDescription option={optionList[optionId - 1]} />
-      </OptionContainer>
-      <CategoryList
-        categories={categories}
-        onClickHandler={(index) => onClickHandler(index as number)}
-        $switch={"option"}
-      />
-    </Container>
+    optionList?.length !== 0 && (
+      <Container>
+        <OptionContainer>
+          <OptionImg
+            src={optionList.find((option) => option.id === optionId)?.imgUrl}
+          />
+          <OptionDescription
+            option={
+              optionList.find((option) => option.id === optionId) as OptionType
+            }
+          />
+        </OptionContainer>
+        <CategoryList
+          categories={categories}
+          onClickHandler={(index) => onClickHandler(index as number)}
+          $switch={"option"}
+        />
+      </Container>
+    )
   );
 }
 
