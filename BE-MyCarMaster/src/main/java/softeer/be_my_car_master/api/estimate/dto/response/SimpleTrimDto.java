a1aebedc922f088f75e449a8e3b8d8c6f5598714
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-18 13:41:37 +0900

    feat: 견적서 조회 response에 필요한 dto 클래스 구현

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/estimate/dto/response/SimpleTrimDto.java
@@ -0,0 +1,29 @@
+package softeer.be_my_car_master.api.estimate.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import softeer.be_my_car_master.domain.trim.Trim;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SimpleTrimDto {
+
+	@Schema(description = "트림 이름", example = "트림 이름")
+	private String name;
+
+	@Schema(description = "트림 기본 비용", example = "40000000")
+	private Integer price;
+
+	public static SimpleTrimDto from(Trim trim) {
+		return SimpleTrimDto.builder()
+			.name(trim.getName())
+			.price(trim.getPrice())
+			.build();
+	}
+}
