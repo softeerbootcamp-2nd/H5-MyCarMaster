969da1919aa42e2f0a8caf651b42e95148d18164
SeoSiun <siun0816@hanyang.ac.kr>
2023-08-20 15:20:00 +0900

    chore: conflict 방지를 위해 기존 port, adaptor 복구

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/infrastructure/jpa/wheel_drive/adaptor/WheelDriveJpaAdaptor.java
@@ -0,0 +1,33 @@
+package softeer.be_my_car_master.infrastructure.jpa.wheel_drive.adaptor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
+import softeer.be_my_car_master.api.wheeldrive.usecase.port.WheelDrivePort;
+import softeer.be_my_car_master.domain.wheel_dirve.WheelDrive;
+import softeer.be_my_car_master.global.annotation.Adaptor;
+import softeer.be_my_car_master.infrastructure.jpa.wheel_drive.entity.TrimWheelDriveEntity;
+import softeer.be_my_car_master.infrastructure.jpa.wheel_drive.repository.EngineUnselectableWheelDriveJpaRepository;
+import softeer.be_my_car_master.infrastructure.jpa.wheel_drive.repository.TrimWheelDriveJpaRepository;
+
+@Adaptor
+@RequiredArgsConstructor
+public class WheelDriveJpaAdaptor implements WheelDrivePort {
+
+	private final TrimWheelDriveJpaRepository trimWheelDriveJpaRepository;
+	private final EngineUnselectableWheelDriveJpaRepository engineUnselectableWheelDriveJpaRepository;
+
+	@Override
+	public List<WheelDrive> findSelectableWheelDrivesByTrimId(Long trimId) {
+		return trimWheelDriveJpaRepository.findAllByTrimId(trimId)
+			.stream()
+			.map(TrimWheelDriveEntity::toWheelDrive)
+			.collect(Collectors.toList());
+	}
+
+	@Override
+	public List<Long> findUnselectableWheelDriveIdsByEngineId(Long engineId) {
+		return engineUnselectableWheelDriveJpaRepository.findUnselectableWheelDriveIdsByEngineId(engineId);
+	}
+}
