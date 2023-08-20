74f73abbaf5caf4ebcc22c75def3092fd7bf6a99
imdh <eastlake520@gmail.com>
2023-08-20 14:07:45 +0900

    feat: 대리점 조회 Port, Adaptor 추가

---
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/api/agency/usecase/get_agencies/GetAgenciesAdaptor.java
@@ -0,0 +1,24 @@
+package softeer.be_my_car_master.api.agency.usecase.get_agencies;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
+import softeer.be_my_car_master.domain.agency.Agency;
+import softeer.be_my_car_master.global.annotation.Adaptor;
+import softeer.be_my_car_master.infrastructure.jpa.agency.entity.AgencyEntity;
+import softeer.be_my_car_master.infrastructure.jpa.agency.repository.AgencyJpaRepository;
+
+@Adaptor
+@RequiredArgsConstructor
+public class GetAgenciesAdaptor implements GetAgenciesPort{
+
+	private final AgencyJpaRepository agencyJpaRepository;
+
+	@Override
+	public List<Agency> findAgenciesInGu(String gu) {
+		return agencyJpaRepository.findAllByGu(gu).stream()
+			.map(AgencyEntity::toAgencyInGu)
+			.collect(Collectors.toList());
+	}
+}
