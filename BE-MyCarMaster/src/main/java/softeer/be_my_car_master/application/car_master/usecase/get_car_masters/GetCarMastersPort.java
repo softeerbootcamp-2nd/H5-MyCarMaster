21f97c323c064beb88a8580f6e1cd16d85c8e3fa
jangdongho <ehdgh25@naver.com>
2023-08-21 16:48:19 +0900

    refactor: 카마스터 조회 로직 수정
    기존에 대리점을 조회하며 연관되어 있던 카마스터를 가져오는 로직에서, 대리점 조회후 대리점 id 목록으로 카마스터 조회

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/application/car_master/usecase/get_car_masters/GetCarMastersPort.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/application/car_master/usecase/get_car_masters/GetCarMastersPort.java
@@ -3,10 +3,13 @@ package softeer.be_my_car_master.application.car_master.usecase.get_car_masters;
 import java.util.List;
 
 import softeer.be_my_car_master.domain.agency.Agency;
+import softeer.be_my_car_master.domain.car_master.CarMaster;
 import softeer.be_my_car_master.global.annotation.Port;
 
 @Port
 public interface GetCarMastersPort {
 
-	List<Agency> findAgenciesAndCarMasters(Double latitude, Double longitude);
+	List<Agency> findAgenciesByLocation(Double latitude, Double longitude);
+
+	List<CarMaster> findCarMastersByAgenciesOrderBySales(List<Long> agencyIds);
 }
