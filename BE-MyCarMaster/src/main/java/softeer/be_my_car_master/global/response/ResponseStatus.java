a81d6761a7a8ef5e971f3c1dcace39942bfd9530
jangdongho <ehdgh25@naver.com>
2023-08-21 15:39:04 +0900

    chore: 예외 설명 명시적으로 변경

--- BE-MyCarMaster/src/main/java/softeer/be_my_car_master/global/response/ResponseStatus.java
+++ BE-MyCarMaster/src/main/java/softeer/be_my_car_master/global/response/ResponseStatus.java
@@ -9,7 +9,7 @@ public enum ResponseStatus {
 
 	BAD_REQUEST(4000, "잘못된 요청입니다"),
 
-	INVALID_OPTION(4100, "선택 불가능한 옵션이 포함되어 있습니다."),
+	INVALID_OPTION_BY_TRIM(4100, "해당 트림에서 선택 불가능한 옵션이 포함되어 있습니다."),
 
 	INVALID_ESTIMATE(4200, "잘못된 견적서입니다. 견적서 내용을 다시 확인해주세요."),
 	INVALID_ESTIMATE_ID(4201, "해당 ID와 일치하는 견적서가 존재하지 않습니다."),
