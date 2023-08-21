46cbfebefc3b819a65e584b6ace6d54b6885bfcf
miniOS <seungmin.oh.dev@gmail.com>
2023-08-21 11:00:10 +0900

    feat: 각 Step의 화면 전환을 담당하는 StepRouter 구현

--- iOS-MyCarMaster/MyCarMaster/MyCarMaster/Scene/Main/Router/StepRouter.swift
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Scene/Main/Router/StepRouter.swift
@@ -1,5 +1,5 @@
 //
-//  Router.swift
+//  StepRouter.swift
 //  MyCarMaster
 //
 //  Created by SEUNGMIN OH on 2023/08/20.
@@ -8,7 +8,8 @@
 import Combine
 import UIKit
 
-final class Router {
+final class StepRouter {
+
     private let currentStepSubject: CurrentValueSubject<Step, Never>
     private var currentStep: Step {
         return currentStepSubject.value
@@ -18,7 +19,7 @@ final class Router {
         .eraseToAnyPublisher()
 
     init(entryStep: Step) {
-        currentStepSubject = CurrentValueSubject(entryStep)
+        self.currentStepSubject = CurrentValueSubject(entryStep)
     }
 
     func nextStep() {
@@ -33,5 +34,31 @@ final class Router {
             fatalError("프로그래밍 오류: 이전 스텝이 존재하지 않습니다.")
         }
         currentStepSubject.send(backStep)
+    }
+
+    func presentEstimation() {
+    }
+}
+
+extension StepRouter {
+    func resolveStepViewController(for step: Step) -> UIViewController {
+        switch step {
+        case .trim:
+            return TrimViewController()
+        case .engine:
+            return EngineViewController()
+        case .wheelDrive:
+            return WheelDriveViewController()
+        case .bodyType:
+            return BodyTypeViewController()
+        case .exterior:
+            return ExteriorViewController()
+        case .interior:
+            return InteriorViewController()
+        case .option:
+            return OptionViewController()
+        case .quotation:
+            return QuotationViewController()
+        }
     }
 }
