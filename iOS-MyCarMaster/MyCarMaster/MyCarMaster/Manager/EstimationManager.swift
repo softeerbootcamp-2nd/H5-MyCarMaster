6db871b7f3ccf52b6e467ac9bae083d1bd9cba0e
miniOS <seungmin.oh.dev@gmail.com>
2023-08-22 05:34:55 +0900

    feat: EstimationManager에 Estimation을 update하는 모든 로직 구현

--- iOS-MyCarMaster/MyCarMaster/MyCarMaster/Manager/EstimationManager.swift
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Manager/EstimationManager.swift
@@ -11,7 +11,16 @@ import MCMCombineExtension
 
 typealias Estimation = Quotation
 
-final class EstimationManager {
+protocol EstimationManageable: AnyObject {
+    var estimation: Estimation { get }
+    var estimationPublisher: AnyPublisher<Estimation, Never> { get }
+
+    func update<T>(_ keyPath: WritableKeyPath<Estimation, T?>, value: T)
+    func update<T: Priceable>(_ keyPath: WritableKeyPath<Estimation, T?>, value: T)
+    func update(_ keyPath: WritableKeyPath<Estimation, [Option]>, value: Option)
+}
+
+final class EstimationManager: EstimationManageable {
 
     private let estimationSubject: CurrentValueRelay<Estimation>
     var estimation: Estimation {
@@ -25,9 +34,48 @@ final class EstimationManager {
         self.estimationSubject = CurrentValueRelay<Estimation>(estimation)
     }
 
-    func update<T>(_ keyPath: WritableKeyPath<Estimation, T>, value: T) {
+    func update<T>(_ keyPath: WritableKeyPath<Estimation, T?>, value: T) {
         var estimation = self.estimation
         estimation[keyPath: keyPath] = value
         estimationSubject.accept(estimation)
+    }
+
+    func update<T: Priceable>(_ keyPath: WritableKeyPath<Estimation, T?>, value: T) {
+        var estimation = self.estimation
+        estimation[keyPath: keyPath] = value
+        estimation.totalPrice = calculateTotalPrice(estimation)
+        estimationSubject.accept(estimation)
+    }
+
+    func update(_ keyPath: WritableKeyPath<Estimation, [Option]>, value: Option) {
+        var estimation = self.estimation
+        if keyPath == \.selectedOptions {
+            estimation.selectedOptions?.insert(value)
+            estimation.consideredOptions?.remove(value)
+        } else {
+            estimation.consideredOptions?.insert(value)
+            estimation.selectedOptions?.remove(value)
+        }
+
+        estimation.selectedOptionsTotalPrice = calculateSelectOptionsTotalPrice(estimation)
+        estimation.totalPrice = calculateTotalPrice(estimation)
+
+        estimationSubject.accept(estimation)
+    }
+
+    private func calculateTotalPrice(_ estimation: Estimation) -> Int {
+        var totalPrice = 0
+        totalPrice += estimation.trim?.price ?? 0
+        totalPrice += estimation.engine?.price ?? 0
+        totalPrice += estimation.bodyType?.price ?? 0
+        totalPrice += estimation.wheelDrive?.price ?? 0
+        totalPrice += estimation.exterior?.price ?? 0
+        totalPrice += estimation.interior?.price ?? 0
+        totalPrice += calculateSelectOptionsTotalPrice(estimation)
+        return totalPrice
+    }
+
+    private func calculateSelectOptionsTotalPrice(_ estimation: Estimation) -> Int {
+        return estimation.selectedOptions?.map(\.price).reduce(0, +) ?? 0
     }
 }
