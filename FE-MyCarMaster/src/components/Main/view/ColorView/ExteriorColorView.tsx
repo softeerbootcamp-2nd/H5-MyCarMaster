55b86538846600b92e6119144fc19c7cd8fcf710
ParkByunggwon <startartart@naver.com>
2023-08-21 23:12:23 +0900

    feat: ExteriorColorView API 연동

--- FE-MyCarMaster/src/components/Main/view/ColorView/ExteriorColorView.tsx
+++ FE-MyCarMaster/src/components/Main/view/ColorView/ExteriorColorView.tsx
@@ -1,13 +1,17 @@
+import styled from "styled-components";
 import {
   useCarPaintDispatch,
   useCarPaintState,
 } from "../../../../contexts/CarPaintContext";
-import { useQuotationDispatch } from "../../../../contexts/QuotationContext";
+import {
+  useQuotationDispatch,
+  useQuotationState,
+} from "../../../../contexts/QuotationContext";
 import { ExteriorColors } from "../../../../types/carpaint.types";
 import { useTrimState } from "../../../../contexts/TrimContext";
 import useFetch from "../../../../hooks/useFetch";
 import { useEffect } from "react";
-import CarRotation from "../../../common/CarRotation/CarRotation";
+// import CarRotation from "../../../common/CarRotation/CarRotation";
 
 interface FetchExteriorProps extends ExteriorColors {
   result: {
@@ -19,7 +23,8 @@ function ExteriorColorView() {
   const SERVER_URL = import.meta.env.VITE_APP_SERVER_URL;
 
   const { trimId } = useTrimState();
-  const { exteriorList } = useCarPaintState();
+  const { exteriorList, exteriorId } = useCarPaintState();
+  const { carPaintQuotation } = useQuotationState();
   const exteriorDispatch = useCarPaintDispatch();
   const quotationDispatch = useQuotationDispatch();
 
@@ -30,33 +35,54 @@ function ExteriorColorView() {
 
   useEffect(() => {
     if (data) {
+      if (carPaintQuotation.exteriorColorQuotation.id) return;
+
       exteriorDispatch({
         type: "SET_EXTERIOR_LIST",
         payload: { exteriorList: data.result.exteriorColors },
+      });
+
+      exteriorDispatch({
+        type: "SELECT_EXTERIOR",
+        payload: { exteriorId: data.result.exteriorColors[0].id },
       });
 
       quotationDispatch({
         type: "SET_CAR_PAINT_QUOTATION",
         payload: {
           type: "exteriorColorQuotation",
+          id: data.result.exteriorColors[0].id,
           name: data.result.exteriorColors[0].name,
           price: data.result.exteriorColors[0].price,
           imgUrl: data.result.exteriorColors[0].colorImgUrl,
         },
       });
     }
-  }, [data, exteriorDispatch]);
+  }, [data]);
 
   if (!exteriorList?.length) return null;
 
-  // 360도 회전에 사용될 이미지 배열을 <CarRotation />로 넘겨줘서 구현
-
   return (
     exteriorList?.length && (
-      // <ExteriorColorImg src={exteriorList[exteriorId - 1].coloredImgUrl} />
-      <CarRotation $isQuotation={false} />
+      <ExteriorColorImg
+        src={
+          exteriorList.find((exterior) => exterior.id === exteriorId)
+            ?.coloredImgUrl
+        }
+      />
+      // <CarRotation $isQuotation={false} />
     )
   );
 }
+
+const ExteriorColorImg = styled.img`
+  width: 100%;
+  max-width: 50rem;
+  margin: 0 auto;
+  height: 100%;
+
+  object-fit: scale-down;
+  object-position: center;
+`;
 
 export default ExteriorColorView;
