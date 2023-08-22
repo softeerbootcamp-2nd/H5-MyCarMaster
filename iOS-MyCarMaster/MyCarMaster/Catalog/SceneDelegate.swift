42d542b1fe10ba3fbac327f2bf9d93a6b0fb66d5
miniOS <seungmin.oh.dev@gmail.com>
2023-08-22 11:30:29 +0900

    feat: 이미지 압축 비교를 위한 Catalog 구현 및 전체 Catalog 정리

--- iOS-MyCarMaster/MyCarMaster/Catalog/SceneDelegate.swift
+++ iOS-MyCarMaster/MyCarMaster/Catalog/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowSchene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowSchene)
-        window?.rootViewController = ViewController()
+        let navigationView = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationView
         window?.makeKeyAndVisible()
     }
 }
--- iOS-MyCarMaster/MyCarMaster/Catalog/ViewController.swift
+++ iOS-MyCarMaster/MyCarMaster/Catalog/ViewController.swift
@@ -10,18 +10,56 @@ import UIKit
 import MCMResource
 
 class ViewController: UIViewController {
+    
+    lazy var componentButton = UIButton().then { button in
+        button.setTitle("컴포넌트 카탈로그", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1.0
+        button.addTarget(self, action: #selector(showComponentView), for: .touchUpInside)
+    }
+    
+    lazy var compressionButton = UIButton().then { button in
+        button.setTitle("이미지 압축 비교", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1.0
+        button.addTarget(self, action: #selector(showCompressionView), for: .touchUpInside)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .MCM.white
         
-        let paddingButton = PaddingButton(vertical: 8, horizontal: 16)
-        paddingButton.style = .buttonTitleSmall(nil)
-        paddingButton.configureUI(titleColor: .MCM.white, backgroundColor: .MCM.gold3)
-        paddingButton.setStyledTitle("Padding Button", for: .normal)
-        paddingButton.frame.origin = CGPoint(x: 100, y: 100)
-        paddingButton.sizeToFit()
-        view.addSubview(paddingButton)
+        let stackView = UIStackView(arrangedSubviews: [
+            componentButton,
+            compressionButton,
+        ]).then { stackView in
+            stackView.axis = .vertical
+            stackView.spacing = 20
+            stackView.alignment = .fill
+            stackView.distribution = .equalCentering
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+//            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    @objc
+    func showComponentView() {
+        let viewController = ComponentViewController()
+        navigationController?.pushViewController(viewController, animated: false)
+    }
+    @objc
+    func showCompressionView() {
+        let viewController = CompressionViewController()
+        navigationController?.pushViewController(viewController, animated: false)
     }
 }
