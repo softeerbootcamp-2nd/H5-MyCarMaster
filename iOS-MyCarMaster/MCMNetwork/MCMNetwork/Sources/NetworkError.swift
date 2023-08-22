c39f871f3d913cb531106f1f4802f7a3f6e3d2d6
miniOS <seungmin.oh.dev@gmail.com>
2023-08-22 17:23:10 +0900

    feat: NetworkProvider의 오류를 확인할 수 있도록 LocalizedError 채택

--- iOS-MyCarMaster/MCMNetwork/MCMNetwork/Sources/NetworkError.swift
+++ iOS-MyCarMaster/MCMNetwork/MCMNetwork/Sources/NetworkError.swift
@@ -14,4 +14,24 @@ public enum NetworkError: Error, Equatable {
     case jsonDecodingError
     case unknown(errorMessage: String)
     case requestMapping(_ url: String)
+    
+}
+
+extension NetworkError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case let .badBaseURL(baseURL):
+            return "badBaseURL: \(baseURL)"
+        case let .badRequestURL(targetDescription):
+            return "badRequestURL: \(targetDescription)"
+        case let .badRequestParameters(parameter):
+            return "badRequestParameter: \(parameter)"
+        case let .jsonDecodingError:
+            return "jsonDecodingError"
+        case let .unknown(errorMessage):
+            return "unkown: \(errorMessage)"
+        case let .requestMapping(url):
+            return "requestMapping \(url)"
+        }
+    }
 }
