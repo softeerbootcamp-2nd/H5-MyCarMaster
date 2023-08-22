c643b4b0aa02e33199c50b765bb7b2cab26d5ecd
ParkByunggwon <startartart@naver.com>
2023-08-22 22:51:18 +0900

    design: 컴포넌트 스타일 파일 생성 및 재정리

---
+++ FE-MyCarMaster/src/components/Header/style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 7.5rem;
+  margin: 0 3%;
+  gap: 60rem;
+`;
+
+export const Img = styled.img`
+  width: 20%;
+  min-width: 12rem;
+  max-width: 16rem;
+  z-index: 100;
+  cursor: pointer;
+`;
+
+export const ModelSelector = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const ModelName = styled.div`
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.NAVYBLUE5};
+`;
+
+export const ModelButton = styled.img`
+  cursor: pointer;
+`;
