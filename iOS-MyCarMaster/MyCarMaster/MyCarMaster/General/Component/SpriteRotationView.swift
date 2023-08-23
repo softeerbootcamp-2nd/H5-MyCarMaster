58ad09b6b9021ddb4b5def2abe9901933c9b0074
miniOS <seungmin.oh.dev@gmail.com>
2023-08-24 02:54:53 +0900

    feat: Sprite 이미지를 360도 돌릴 수 있는 Component 구현

---
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/General/Component/SpriteRotationView.swift
@@ -0,0 +1,130 @@
+//
+//  SpriteRotationView.swift
+//  MyCarMaster
+//
+//  Created by SEUNGMIN OH on 2023/08/24.
+//
+
+import UIKit
+
+final class SpriteRotationView: UIView {
+    
+    // MARK: Property
+    
+    /// 스크롤 대상이 되는 세로로 긴 이미지
+    private let spriteImage: UIImage
+    
+    /// SpriteImage 내부에 있는 사진의 개수
+    private let imageCount: Int
+    
+    /// 가로로 한 번 스크롤 했을 때, 회전할 수 있는 바퀴 수
+    /// 0 이하의 값일 때에는, 1로 설정된다.
+    var sensitive: CGFloat
+    
+    /// ImageViewHeight / ImageCount 만큼의 높이를 기준으로 동작한다.
+    /// ImageSize를 기준으로 설정하면, float의 정밀도 때문에 오차가 발생한다.
+    private var imageSize: CGSize {
+        return CGSize(width: imageView.frame.size.width, height: imageView.frame.size.height / CGFloat(imageCount))
+    }
+    
+    private var wholeImageRatio: CGFloat {
+        return spriteImage.size.height / spriteImage.size.width
+    }
+    
+    override var description: String {
+        return """
+        Bounds: \(bounds)
+        ImageViewSize: \(imageSize)
+        ImageViewImageSize: \(imageView.image!.size)
+        ImageSize: \(spriteImage.size)
+        WholeImageRatio: \(wholeImageRatio)
+        OneImageRatio: \(imageSize.height / imageSize.width)
+        """
+    }
+    
+    /// 스크롤이 시작하는 이미지의 Y값을 나타낸다.
+    private var startingY: CGFloat = 0
+    
+    // MARK: View
+    private lazy var imageView = UIImageView().then { imageView in
+        imageView.image = spriteImage
+        imageView.contentMode = .scaleAspectFill
+    }
+    
+    init(imageCount: Int, sensitive: CGFloat, image: UIImage) {
+        self.imageCount = imageCount
+        self.sensitive = sensitive
+        self.spriteImage = image
+        
+        super.init(frame: .zero)
+        configureUI()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        clipsToBounds = true
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 1.0
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    private func configureLayout() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(imageView)
+        
+        [imageView].forEach { subview in
+            subview.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: wholeImageRatio),
+            heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1 / CGFloat(imageCount)),
+        ])
+    }
+    
+    @objc
+    private func handlePan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self)
+        
+        // (0, sensitive)의 값을 갖는데, 1을 360도라고 생각한다.
+        let diff = (translation.x / self.bounds.width) * sensitive
+        
+        switch sender.state {
+        case .began:
+            startingY = self.bounds.origin.y
+        case .changed:
+            let imageNext = Int(round(diff * CGFloat(imageCount))) // (360 * diff) / (360 / imageCount)
+            print("imageNext:",imageNext)
+            
+            // y를 몇 옮겨야 할까?
+            let lastImageY = imageView.bounds.height - imageSize.height
+            var windowY = startingY + CGFloat(imageNext) * imageSize.height
+            // y의 최소/최대 범위는 어떻게 처리할까?
+            if windowY < 0 {
+                windowY += lastImageY
+            } else if lastImageY < windowY {
+                windowY -= lastImageY
+            }
+            
+            // 옮기기
+            self.bounds = CGRect(
+                origin: CGPoint(x: self.bounds.origin.x, y: windowY),
+                size: self.bounds.size
+            )
+        case .ended:
+            startingY = self.bounds.origin.y
+        default:
+            break
+        }
+    }
+}
