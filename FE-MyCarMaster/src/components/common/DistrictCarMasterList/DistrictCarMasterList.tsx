c1145acbc9977172941d413a128d1bcecb3e38bc
wonho <59119468+wonho1401@users.noreply.github.com>
2023-08-23 18:30:18 +0900

    [Fix] 빌드 오류 수정 (#372)
    * fix: 견적서 조회 시 useParams 수정
    
    * fix: 작성된 견적서 URL 변경
    /{estimateId} -> /estimates/{estimateId}
    
    * chore: 확정시 변경 불가 멘트 추가
    
    * chore: 서울 지도 파일 경로 변경
    
    * chore: console.log 삭제
    
    * fix: build 오류 수정

--- FE-MyCarMaster/src/components/common/DistrictCarMasterList/DistrictCarMasterList.tsx
+++ FE-MyCarMaster/src/components/common/DistrictCarMasterList/DistrictCarMasterList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { get } from "../../../utils/fetch";
 import theme from "../../../styles/Theme";
