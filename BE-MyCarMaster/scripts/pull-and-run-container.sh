0e0cba7214a26220a7cc8712455d7c0f65f8382d
imdh <eastlake520@gmail.com>
2023-08-23 13:05:10 +0900

    [Setting] 로그 관리를 위한 설정 (#363)
    * setting: appspec.yml에 yml파일 복사 내용 추가
    
    * feat: scripts 폴더에 promtail-config.yml 추가
    
    * feat: promtail run 명령어 추가

--- BE-MyCarMaster/scripts/pull-and-run-container.sh
+++ BE-MyCarMaster/scripts/pull-and-run-container.sh
@@ -6,5 +6,11 @@ sudo docker pull softeerh5/my-car-master
 # 도커 run
 sudo docker run -d -p 8080:8080 -p 9292:9292 --name my-car-master softeerh5/my-car-master:latest
 
+sudo docker run -d -p 9080:9080 \
+    --name promtail \
+    -v ./promtail-config.yml:/etc/promtail/promtail-config.yml \
+    -v ./logs/:/logs/ \
+    grafana/promtail:latest -config.file=/etc/promtail/promtail-config.yml
+
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제 안됨
 sudo docker rmi -f $(docker images -f "dangling=true" -q) || true
