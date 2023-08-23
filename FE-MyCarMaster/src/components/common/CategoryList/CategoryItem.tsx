d5f7be775d6764f57dbfbc36d9ae0f4aee94e666
ParkByunggwon <startartart@naver.com>
2023-08-24 01:37:25 +0900

    refactor: Category Text 적용 및 수정

--- FE-MyCarMaster/src/components/common/CategoryList/CategoryItem.tsx
+++ FE-MyCarMaster/src/components/common/CategoryList/CategoryItem.tsx
@@ -1,25 +1,36 @@
 import styled from "styled-components";
+import { FontType } from "@/styles/Theme";
 
 type CategoryItemProp = {
   name?: string;
   $active?: boolean;
+  $font?: FontType;
   onClickHandler?: () => void;
 };
 
-function CategoryItem({ name, $active, onClickHandler }: CategoryItemProp) {
+function CategoryItem({
+  name,
+  $active,
+  $font,
+  onClickHandler,
+}: CategoryItemProp) {
   return (
     <Container onClick={onClickHandler}>
-      <Text $active={$active}>{name}</Text>
+      <Text $active={$active} $font={$font}>
+        {name}
+      </Text>
     </Container>
   );
 }
 
 const Container = styled.li`
   cursor: pointer;
+  
 `;
 
 const Text = styled.p<CategoryItemProp>`
-  font-size: 1.5rem;
+  ${(props) => props.$font};
+  font-size: ${(props) => props.$active && props.$font?.Active?.fontSize};
   color: ${(props) => (props.$active ? "#1A3276" : "#C5C9D2")};
   border-bottom: ${(props) => props.$active && "2px solid #1A3276"};
 `;
