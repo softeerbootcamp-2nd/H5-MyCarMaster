83013c06e1cf0bf09bc726d5a1b752dcbd5efd9e
wonho1401 <wonho1401@naver.com>
2023-08-23 13:35:31 +0900

    chore: 확정시 변경 불가 멘트 추가

--- FE-MyCarMaster/src/components/common/Modals/ModalContents/ConfirmModal.tsx
+++ FE-MyCarMaster/src/components/common/Modals/ModalContents/ConfirmModal.tsx
@@ -1,7 +1,12 @@
 import { styled } from "styled-components";
 
 function ConfirmModal() {
-  return <Container>견적서를 확정하시겠습니까?</Container>;
+  return (
+    <Container>
+      견적서를 확정하시겠습니까?
+      <P /> 확정하시면 선택하신 것을 변경할 수 없습니다.
+    </Container>
+  );
 }
 
 export default ConfirmModal;
@@ -19,4 +24,8 @@ const Container = styled.div`
   font-size: 1.5rem;
   font-style: normal;
   font-weight: 500;
+`;
+
+const P = styled.p`
+  margin: 1rem;
 `;
