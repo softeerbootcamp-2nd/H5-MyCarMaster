cbebdd8dca824fb8ebcefbcd9b35c64a3afff9a2
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2023-08-24 13:00:43 +0900

    [FE-Bug] Font Recall 개선 (#387)
    * feat: useFont hook 구현
    FontFace 클래스 사용, Document에 저장
    
    * chore: fonts 상수 파일 및 등록
    
    * feat: App.tsx 적용 및 확인
    Network에서 받아오는 폰트 확인 결과 개선 완료

---
+++ FE-MyCarMaster/src/constants/Font.constants.ts
@@ -0,0 +1,23 @@
+export const fonts = [
+  {
+    family: "HyundaiSansBold",
+    url: "./fonts/HyundaiSansHeadKRBold.woff2",
+    options: {
+      fontDisplay: "swap",
+    },
+  },
+  {
+    family: "HyundaiSansMedium",
+    url: "./fonts/HyundaiSansHeadKRMedium.woff2",
+    options: {
+      fontDisplay: "swap",
+    },
+  },
+  {
+    family: "HyundaiSansRegular",
+    url: "./fonts/HyundaiSansHeadKRRegular.woff2",
+    options: {
+      fontDisplay: "swap",
+    },
+  },
+];
--- FE-MyCarMaster/src/main.tsx
+++ FE-MyCarMaster/src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import GlobalStyle from "./styles/GlobalStyle.ts";
 import { BrowserRouter } from "react-router-dom";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
