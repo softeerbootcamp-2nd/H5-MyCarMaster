d3220d61ea5a6ffaccd7dffc7aab77a624a51fea
miniOS <seungmin.oh.dev@gmail.com>
2023-08-27 18:28:57 +0900

    feat: EngineReactor 적용

--- iOS-MyCarMaster/MyCarMaster/MyCarMaster/Scene/Step/Engine/EngineViewController.swift
+++ iOS-MyCarMaster/MyCarMaster/MyCarMaster/Scene/Step/Engine/EngineViewController.swift
@@ -5,6 +5,7 @@
 //  Created by SEUNGMIN OH on 2023/08/11.
 //
 
+import Combine
 import UIKit
 
 import MCMNetwork
@@ -13,15 +14,36 @@ import MVIFoundation
 final class EngineViewController: UIViewController {
 
     typealias ListCellClass = BasicListCell
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Engine>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Engine>
 
-    var dataList: [Engine] = []
-
-    var selectedCellIndexPath: IndexPath = IndexPath(row: 0, section: 0) {
-        didSet {
-            print(#function, selectedCellIndexPath)
-        }
+    enum Section {
+        case engine
     }
 
+    // MARK: Property
+    var cancellables = Set<AnyCancellable>()
+    
+    var selectedEngine: Engine?
+    
+    private var dataSource: DataSource!
+    private func configureDataSource() {
+        dataSource = DataSource(
+            collectionView: contentView.listView,
+            cellProvider: { collectionView, indexPath, itemIdentifier in
+                guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: ListCellClass.reuseIdentifier,
+                    for: indexPath
+                ) as? ListCellClass else {
+                    fatalError("개발자 오류: 등록되지 않은 Cell 입니다.")
+                }
+                cell.configure(with: itemIdentifier)
+                
+                return cell
+            })
+        contentView.setDataSource(dataSource)
+    }
+    
     private var contentView: EngineView<ListCellClass> {
         return view as? EngineView ?? EngineView()
     }
@@ -32,17 +54,13 @@ final class EngineViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDataSource()
         configureUI()
-#if ONLINE
-        fetchData()
-#elseif OFFLINE
-        fetchFromDisk()
-#endif
+        reactor?.action.send(.viewDidLoad)
     }
 
     private func configureUI() {
         contentView.setDelegate(self)
-        contentView.setDataSource(self)
     }
 
     override func didMove(toParent parent: UIViewController?) {
@@ -50,85 +68,74 @@ final class EngineViewController: UIViewController {
     }
 }
 
-extension EngineViewController {
-    private func fetchFromDisk() {
-        guard let fileURL = Bundle.main.url(forResource: "EngineDTO.json", withExtension: nil) else { return }
-        applyData(try? Data(contentsOf: fileURL))
-    }
-
-    private func fetchData() {
-        let request = URLRequest(url: URL(string: Dependency.serverURL + "engines?trimId=1")!)
-
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                print(error?.localizedDescription)
-                return
-            }
-
-            guard let response = response as? HTTPURLResponse else {
-                print("Sever Error")
-                return
-            }
-
-            print("Engine:", response.statusCode)
-            guard 200..<300 ~= response.statusCode else {
-                return
-            }
-
-            self.applyData(data)
-        }.resume()
-    }
-
-    private func applyData(_ data: Data?) {
-        if let data,
-           let engineDTOList = try? JSONDecoder().decode(RootDTO.self, from: data).result.engines {
-            self.dataList = engineDTOList.map { Engine($0) }
-            DispatchQueue.main.async {
-                self.contentView.listView.reloadData()
-            }
-        } else {
-            print("Decoding Error")
-            return
+extension EngineViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let engine = dataSource.itemIdentifier(for: indexPath) {
+            reactor?.action.send(.engineDidSelect(engine))
         }
     }
 }
 
-extension EngineViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-
-    func collectionView(
-        _ collectionView: UICollectionView,
-        numberOfItemsInSection section: Int
-    ) -> Int {
-        return dataList.count
+extension EngineViewController: Reactable {
+    func bindState(reactor: EngineReactor) {
+        reactor.state.map(\.selectedEngine)
+            .dropFirst()
+            .sink { [weak self] engine in
+                if let engine {
+                    self?.selectItemFor(engine)
+                    return
+                }
+                
+                if let firstEngine = self?.dataSource.itemIdentifier(
+                    for: .init(item: 0, section: 0)
+                ) {
+                    reactor.action.send(.engineDidSelect(firstEngine))
+                }
+                return
+            }
+            .store(in: &cancellables)
+        
+        reactor.state.map(\.engineList)
+            .dropFirst()
+            .removeDuplicates()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] engineList in
+                var snapshot = Snapshot()
+                snapshot.appendSections([.engine])
+                snapshot.appendItems(engineList)
+                self?.dataSource.apply(snapshot, completion: {
+                    reactor.action.send(.dataSourceDidApply)
+                })
+            }
+            .store(in: &cancellables)
+        
+        reactor.state.compactMap(\.errorDescription)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] errorDescription in
+                let alert = UIAlertController(
+                    title: "에러",
+                    message: errorDescription,
+                    preferredStyle: .alert
+                )
+                alert.addAction(.init(title: "확인", style: .default))
+                print(errorDescription)
+                self?.present(alert, animated: false)
+            }
+            .store(in: &cancellables)
     }
+}
 
-    func collectionView(
-        _ collectionView: UICollectionView,
-        cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
-
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: ListCellClass.reuseIdentifier,
-            for: indexPath
-        ) as? ListCellClass else {
-            fatalError("등록되지 않은 cell입니다.")
-        }
-
-        cell.configure(with: dataList[indexPath.row])
-
-        // 프리셋을 선택한다.
-        if selectedCellIndexPath == indexPath {
-            collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
-        }
-
-        return cell
+extension EngineViewController {
+    func selectItemFor(_ engine: Engine) {
+        guard let indexPath = dataSource.indexPath(for: engine) else { return }
+        selectItemAt(indexPath)
     }
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? ListCellClass else {
-            fatalError("알 수 없는 오류가 발생했습니다.")
+    
+    func selectItemAt(_ indexPath: IndexPath) {
+        guard dataSource?.itemIdentifier(for: indexPath) != nil else {
+            print("Error: \(indexPath)에 원소가 없음")
+            return
         }
-        guard selectedCellIndexPath != indexPath else { return }
-        selectedCellIndexPath = indexPath
+        contentView.listView.selectItem(at: indexPath, animated: true, scrollPosition: [.centeredVertically])
     }
 }
