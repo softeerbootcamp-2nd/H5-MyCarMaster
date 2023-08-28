ae542ab59f8ca7aa680cffde650e302a5238dcd0
miniOS <seungmin.oh.dev@gmail.com>
2023-08-28 12:51:26 +0900

    test: imageURL을 ios전용 url로 바꾸는 로직 테스트

--- iOS-MyCarMaster/MyCarMaster/MyCarMasterTests/ImageUrlReplacingTests.swift
+++ iOS-MyCarMaster/MyCarMaster/MyCarMasterTests/ImageUrlReplacingTests.swift
@@ -1,8 +1,19 @@
 //
-//  imageUrlReplacingTests.swift
+//  ImageUrlReplacingTests.swift
 //  MyCarMasterTests
 //
 //  Created by SEUNGMIN OH on 2023/08/28.
 //
 
-import Foundation
+@testable import MyCarMaster
+import XCTest
+
+final class ImageUrlReplacingTests: XCTestCase {
+   
+    func testReplacing() throws {
+        let origin = "https://beta.my-car-master.shop/images/palisade/trim/calligraphy.png"
+        let expected = "https://beta.my-car-master.shop/images/palisade/trim/calligraphy_ios.png"
+        
+        XCTAssertEqual(origin.iOS, expected)
+    }
+}
